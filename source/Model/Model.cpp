2a28de4beddbd1fcfa7ce89fa566dd4012b8ca53
Scott Rasmussen (Zaita) <scott@zaita.com>
2012-12-06 03:45:24 +0000

    ADD: Some new translations for model parameters CHG: Model now supports some real parameters ADD: some configuration files for unit testing

--- source/Model/Model.cpp
+++ source/Model/Model.cpp
@@ -2,8 +2,10 @@
  * @file Model.cpp
  * @author  Scott Rasmussen (scott.rasmussen@zaita.com)
  * @version 1.0
- * @date 16/11/2012
+ * @date 6/12/2012
  * @section LICENSE
+ *
+ * Copyright NIWA Science ©2012 - www.niwa.co.nz
  *
  * $Date: 2008-03-04 16:33:32 +1300 (Tue, 04 Mar 2008) $
  */
@@ -24,7 +26,13 @@ using std::endl;
 
 Model::Model() {
   LOG_TRACE();
-  parameters_.RegisterAllowed(PARAM_CATEGORIES);
+  parameters_.RegisterAllowed(PARAM_START_YEAR);
+  parameters_.RegisterAllowed(PARAM_RUN_LENGTH);
+  parameters_.RegisterAllowed(PARAM_MIN_AGE);
+  parameters_.RegisterAllowed(PARAM_MAX_AGE);
+  parameters_.RegisterAllowed(PARAM_AGE_PLUS);
+  parameters_.RegisterAllowed(PARAM_INITIALIZATION_PHASES);
+  parameters_.RegisterAllowed(PARAM_TIME_STEPS);
 }
 
 /**
