9ab069d6a829e6030526cff5729b5a32cc1bd34c
Scott Rasmussen (Zaita) <scott@zaita.com>
2012-12-16 21:49:10 +0000

    ADD: Started to add validation for model and recruitment process ADD: Some more error handling when objects have not been defined on the configuration file

--- source/Processes/Children/ConstantRecruitment.cpp
+++ source/Processes/Children/ConstantRecruitment.cpp
@@ -1,22 +1,62 @@
-/*
- * ConstantRecruitment.cpp
+/**
+ * @file ConstantRecruitment.cpp
+ * @author  Scott Rasmussen (scott.rasmussen@zaita.com)
+ * @version 1.0
+ * @date 14/12/2012
+ * @section LICENSE
  *
- *  Created on: 18/09/2012
- *      Author: Admin
+ * Copyright NIWA Science ©2012 - www.niwa.co.nz
+ *
+ * $Date: 2008-03-04 16:33:32 +1300 (Tue, 04 Mar 2008) $
  */
 
+// Headers
 #include "ConstantRecruitment.h"
 
+// Namespaces
 namespace isam {
 namespace processes {
 
+/**
+ * Default Constructor
+ */
 ConstantRecruitment::ConstantRecruitment() {
-  // TODO Auto-generated constructor stub
+  parameters_.RegisterAllowed(PARAM_TYPE);
+  parameters_.RegisterAllowed(PARAM_CATEGORIES);
+  parameters_.RegisterAllowed(PARAM_AGES);
+  parameters_.RegisterAllowed(PARAM_R0);
+}
+
+/**
+ * Destructor
+ */
+ConstantRecruitment::~ConstantRecruitment() {
+}
+
+/**
+ * Validate the parameters for this process
+ */
+void ConstantRecruitment::Validate() {
+
+  // Check for the required parameters
+  CheckForRequiredParameter(PARAM_CATEGORIES);
+  CheckForRequiredParameter(PARAM_AGES);
+  CheckForRequiredParameter(PARAM_R0);
+}
+
+/**
+ * Build any runtime relationships we might
+ * have to other objects in the system.
+ */
+void ConstantRecruitment::Build() {
 
 }
 
-ConstantRecruitment::~ConstantRecruitment() {
-  // TODO Auto-generated destructor stub
+/**
+ *
+ */
+void ConstantRecruitment::Execute() {
+
 }
 
 } /* namespace processes */
--- source/Processes/Manager.cpp
+++ source/Processes/Manager.cpp
@@ -7,6 +7,8 @@
 
 #include "Manager.h"
 
+#include "Utilities/Logging/Logging.h"
+
 namespace isam {
 namespace processes {
 
@@ -15,8 +17,22 @@ Manager::Manager() {
 
 }
 
-Manager::~Manager() {
+Manager::~Manager() noexcept(true) {
   // TODO Auto-generated destructor stub
+}
+
+/**
+ * Validate any loaded processes we have.
+ */
+void Manager::Validate() {
+
+  if (objects_.size() == 0)
+    LOG_ERROR("The configuration file requires you specify at least one type of process. E.g @recruitment, @mortality, @ageing");
+
+  for(ProcessPtr process : objects_) {
+    process->Validate();
+  }
+
 }
 
 } /* namespace processes */
