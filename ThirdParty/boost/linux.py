f255d1c4a49b0430fe5a982d4d06f14fe83a3d92
Scott Rasmussen (Zaita) <scott@zaita.com>
2013-05-08 22:51:22 +0000

    FIX: Linux build scripts have no been updated

--- ThirdParty/boost/linux.py
+++ ThirdParty/boost/linux.py
@@ -3,39 +3,43 @@ import os.path
 import shutil
 from distutils import dir_util
 
-# Variables
-boostFileName = 'boost_1_53_0'
-targetPath    = os.getenv('isam_third_party_target_directory')
+import Globals
 
-# Clean our any existing files if they already exist
-print '-- Cleaning Boost files'
-if os.path.exists(boostFileName):
-    shutil.rmtree(boostFileName)
+def doBuild():
+    # Variables
+    boostFileName = 'boost_1_53_0'
+    targetPath    = Globals.target_path_
 
-# Make a copy of boost.tar.gz as temp.tar.gz if one doesn't exist
-if not os.path.exists('temp.tar') and not os.path.exists('temp.tar.gz'):
-    print '-- Copying Boost archive'
-    os.system('cp ' + boostFileName + '.tar.gz temp.tar.gz')
+    # Clean our any existing files if they already exist
+    print '-- Cleaning Boost files'
+    if os.path.exists(boostFileName):
+        shutil.rmtree(boostFileName)
 
-# Decompress our boost archive
-print '-- Decompressing Boost'
-if os.path.exists('temp.tar.gz'):
-    os.system('gzip -d temp.tar.gz')
+    # Make a copy of boost.tar.gz as temp.tar.gz if one doesn't exist
+    if not os.path.exists('temp.tar') and not os.path.exists('temp.tar.gz'):
+        print '-- Copying Boost archive'
+        os.system('cp ' + boostFileName + '.tar.gz temp.tar.gz')
 
-if os.path.exists('temp.tar'):
-    os.system('tar -x -ftemp.tar')
-    os.system('rm -rf temp.tar')
+    # Decompress our boost archive
+    print '-- Decompressing Boost'
+    if os.path.exists('temp.tar.gz'):
+        os.system('gzip -d temp.tar.gz')
 
-# Build Boost
-print '-- Building Boost - check isam_build.log for progress - estimated time 30 minutes'
-os.chdir(boostFileName)
+    if os.path.exists('temp.tar'):
+        os.system('tar -x -ftemp.tar')
+        os.system('rm -rf temp.tar')
 
-os.system('./bootstrap.sh gcc 1> isam_bootstrap.log 2>&1')
-os.system('./b2 --toolset=gcc link=static runtime-link=static threading=multi 1> isam_build.log 2>&1')
-    
-# Move our headers and libraries
-print '-- Moving headers and libraries'
-dir_util.copy_tree('boost', targetPath + '/include/boost/')
-shutil.copy('stage/lib/libboost_program_options.a', targetPath + "/lib/")
+    # Build Boost
+    print '-- Building Boost - check isam_build.log for progress - estimated time 30 minutes'
+    os.chdir(boostFileName)
 
-os.system('touch ' + targetPath + '/boost.success')
+    os.system('./bootstrap.sh gcc 1> isam_bootstrap.log 2>&1')
+    os.system('./b2 --toolset=gcc link=static runtime-link=static threading=multi 1> isam_build.log 2>&1')
+        
+    # Move our headers and libraries
+    print '-- Moving headers and libraries'
+    dir_util.copy_tree('boost', targetPath + '/include/boost/')
+    shutil.copy('stage/lib/libboost_program_options.a', targetPath + "/lib/")
+
+    os.system('touch ' + targetPath + '/boost.success')
+    return True
---
+++ ThirdParty/parser/linux.py
@@ -0,0 +1,37 @@
+import os
+import os.path
+import subprocess
+import sys
+import shutil
+
+import Globals
+
+def doBuild():
+  # Variables
+  fileName = 'parser'
+  targetPath      = Globals.target_path_
+  
+  # Clean our any existing files if they already exist
+  print '-- Cleaning Parser files'
+  if os.path.exists(fileName):
+      shutil.rmtree(fileName)
+  
+  # Decompress our archive
+  print '-- Decompressing Parser - check isam_unzip.log'
+  if os.path.exists(fileName + '.zip'):
+      os.system('unzip ' + fileName + '.zip 1> isam_unzip.log 2>&1')
+  
+  # Build 
+  print '-- Building Parser - check parser/isam_build.log'
+  os.chdir(fileName)
+  os.system("g++ -g0 -c parser.cpp 1> isam_build.log 2>&1")
+  os.system("ar rvs parser.a parser.o 1>> isam_build.log 2>&1")
+      
+  # Move our headers and libraries
+  print '-- Moving headers and libraries'
+  shutil.copy('parser.a', targetPath + "/lib/")
+  shutil.copy('parser.h', targetPath + "/include/")
+  
+  # Finally touch the success file to create it
+  os.system('touch ' + targetPath + '/parser.success')
+  return True
