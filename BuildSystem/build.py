26845904a132468a457ea39ca3ab0128cdbb4b36
Scott Rasmussen (Zaita) <scott@zaita.com>
2013-06-30 23:31:57 +0000

    CHG: Remamed test.py in to build.py to move to the new build system CHG: Minimum CMake version is now 2.8

--- BuildSystem/build.py
+++ BuildSystem/build.py
@@ -4,129 +4,193 @@ import os.path
 import subprocess
 import fileinput
 
-# Get some system variables
-operatingSystem = sys.platform
-oldPath = os.getenv('PATH')
-osPath = ''
-cwd = os.path.normpath(os.getcwd())
+sys.path.insert(0, "buildtools/classes")
 
-EX_OK = getattr(os, "EX_OK", 0)
+# These are iSAM specific python objects
+from System import *
+from Globals import *
+from Builder import *
 
-if operatingSystem.startswith('linux'):
-  operatingSystem = 'linux'
+"""
+Print the usage for this build system
+"""
+def print_usage():  
+  os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+  print '###########################################################'
+  print '# iSAM Build System Usage                                 #'
+  print '###########################################################'
+  print 'Usage:'
+  print 'doBuild <build_target> <build_parameter>'
+  print ''
+  print 'Valid Build Types:'
+  print '  debug - Build debug version'
+  print '  release - Build release version'
+  print '  thirdparty - Build all required third party libraries'
+  print '  all - Does clean, thirdparty, debug, release builds in order'
+  print '  clean - Remove any previous debug/release build information'
+  print '  cleanall - Remove all previous build information'
+  print ''
+  print 'Valid Build Parameters: (thirdparty only)'
+  print '  <libary name> - Target third party library to build or rebuild'
+  print ''
+  print 'Valid Build parameters: (debug/release only)'
+  print '  admb - Use ADMB auto-differentiation in compiled executable'
 
-# Modify our system PATH environment variable based on OS
-if operatingSystem == 'win32':        
-  osPath += cwd + '\\buildtools\\windows\\unixutils;'
-  osPath += cwd + '\\buildtools\\windows\\cmake\\bin;'   
-  osPath += cwd + '\\buildtools\\windows\\Python27\\;'   
+
+"""
+Start our build system. This method essentially just
+gets all of the system information we need and will
+test that executables are in the path that we need
+"""
+def start_build_system():
+  system_info.set_new_path()
+  if not system_info.find_compiler_path():
+    return False 
+  return True  
+
+"""
+Start The main execution here:
+"""
+system_info = SystemInfo()
+if not start_build_system():
+  system_info.reset_original_path()
+  exit(1)
+
+"""
+Get the build information from the user
+"""
+def start():
+  build_target = ""
+  build_parameters = ""
+  
+  allowed_build_targets = [ "debug", "release", "thirdparty", "test", "all", "clean", "cleanall", "help" ]
+ 
+  if not len(sys.argv) > 1:  
+    os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+    print "*************************************************************************"
+    print "*************************************************************************"
+    print "Welcome to the iSAM Build System"
+    print "*************************************************************************"
+    print "*************************************************************************"
+    print "\n"
+    print "Valid build targets: " +  ", ".join(allowed_build_targets)
+    build_target = raw_input("Please enter a valid build type [debug]: ").lower()
+    while build_target != "" and not build_target in allowed_build_targets:
+      print "## Error: " + build_target + " is not a valid build target" 
+      build_target = raw_input("Please enter a valid build type [debug]: ").lower()
+    if build_target == "":
+      build_target = "debug" 
+    if build_target == "help":
+      print_usage()
+      return True
+  
+    # Find our build parameters to use, this will enable admb etc
+    if build_target == "debug" or build_target == "release":    
+      print "Valid build parameters: " + ", ".join(allowed_build_parameters)
+      build_parameters = raw_input("Please enter build parameters [none]: ").lower()
+      while build_parameters != "" and not build_parameters in allowed_build_parameters:
+        print "## Error: " + build_parameters + " is not a valid build parameter"
+        build_parameters = raw_input("Please enter build parameters [none]: ").lower()
+      if build_parameters == "none":
+        build_parameters = ""  
+  
+  """
+  Handle build information already passed in
+  """
+  if len(sys.argv) > 1 and len(str(sys.argv[1])) > 1:
+      build_target = sys.argv[1]
+  if len(sys.argv) > 2 and len(str(sys.argv[2])) > 1:
+      build_parameters = sys.argv[2] 
+   
+  if build_target == "" or not build_target.lower() in allowed_build_targets:
+    return Globals.PrintError(build_target + " is not a valid build target")
     
-newPath = osPath + oldPath
-os.environ['PATH'] = newPath
+  build_target = build_target.lower()    
+  if build_target == "help":
+    print_usage()
+    return True
 
-# Find our Compiler and put it's location in the path
-compiler = 'g++'
-if operatingSystem == 'win32':
-    compiler += '.exe'
-
-if os.system('which ' + compiler + ' > compiler.txt') != EX_OK:
-    print '## ERROR: Failed to execute \'which\' command to find the currently installed compiler'
-    sys.exit(1)
-
-fi = fileinput.FileInput('compiler.txt')
-compilerPath = ''
-for line in fi:
-    compilerPath = line
-
-compilerPath = compilerPath.replace(compiler + '\n', '').rstrip()
-osPath += compilerPath + ';'
-
-if os.system('rm -rf compiler.txt') != EX_OK:
-    print '## ERROR: Failed to remove the compiler.txt after reading the compiler path'
-    sys.exit(1)
-
-# Find cmd.exe and put it's locaiton in the path
-if operatingSystem == 'win32':
-    if os.system('which cmd.exe > cmd.txt') != EX_OK:
-        print '## ERROR: Failed to execute \'which\' command to find the currently installed cmd.exe path'
-        sys.exit(1)
+  if build_parameters != "": 
+    build_parameters = build_parameters.lower()
+  
+  Globals.build_target_ = build_target
+  Globals.build_parameters_ = build_parameters
+  
+  print " -- Build target: " + Globals.build_target_
+  print " -- Build parameters: " + Globals.build_parameters_
+  
+  if build_target == "debug" or build_target == "release":
+    allowed_build_parameters = [ "", "admb" ]    
+    if not build_parameters in allowed_build_parameters:
+      return Globals.PrintError("Build parameter " + build_parameters + " is not valid")
     
-    fi = fileinput.FileInput('cmd.txt')
-    cmdPath = ''
-    for line in fi:
-        cmdPath = line
+    os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+    print "*************************************************************************"
+    print "*************************************************************************"
+    print "--> Starting " + Globals.build_target_ + " Build"
+    code_builder = MainCode()
+    if not code_builder.start():
+      return False
+  elif build_target == "thirdparty":
+    os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+    print "*************************************************************************"
+    print "*************************************************************************"
+    print "--> Starting " + Globals.build_target_ + " Build"
+    code_builder = ThirdPartyLibraries()
+    if not code_builder.start():
+      return False      
+      
+"""
+This is the entry point in to our build system
+"""
+exit_code = 0
+if not start():
+  exit_code = 1
+  
+system_info.reset_original_path()
+print "--> Finished"
+exit(exit_code)  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
 
-    cmdPath = cmdPath.replace('cmd.exe\n', '').rstrip()
-    osPath += cmdPath + ';'   
-
-    if os.system('rm -rf cmd.txt') != EX_OK:
-        print '## ERROR: Failed to remove the cmd.txt after reading the cmd.exe path'
-        sys.exit(1)
-
-# Change The Path
-if operatingSystem != 'win32':
-    osPath += newPath
-os.environ['PATH'] = osPath 
-    
-# Figure out what kind of build we need to do
-buildType  = "DEBUG"
-buildParam = ""
-
-if len(sys.argv) > 1 and len(str(sys.argv[1])) > 1:
-    buildType = sys.argv[1]
-if len(sys.argv) > 2 and len(str(sys.argv[2])) > 1:
-    buildParam = sys.argv[2]
-
-if buildType.upper() == "HELP" or buildType == "?":
-    print '###########################################################'
-    print '# iSAM Build System                                       #'
-    print '###########################################################'
-    print 'Usage:'
-    print 'doBuild <buildType> <buildParam>'
-    print ''
-    print 'Valid Build Types:'
-    print '  clean - Remove any previous debug/release build information'
-    print '  cleanall - Remove all previous build information'
-    print '  thirdparty - Builds required third party libraries'
-    print '  debug - Builds a debug version of iSAM (default)'
-    print '  release - Builds a release version of iSAM'
-    print ''
-    print 'Valid Build Parameters: (thirdparty only)'
-    print '  <libary name> - Target third party library to build or rebuild'
-    print ''
-    print 'Valid Build parameters: (debug/release only)'
-    print '  admb - Use ADMB auto-differentiation in compiled executable'
-    sys.exit()    
-    
-if buildType.upper() != "DEBUG" and buildType.upper() != "RELEASE" and buildType.upper() != "THIRDPARTY" and buildType.upper() != "CLEAN" and buildType.upper() != "CLEANALL":
-    print 'Invalid build type ' + buildType + '. Supported build types are "release", "debug", "clean", "cleanall" and "thirdparty"'
-    sys.exit()    
-    
-os.putenv('isam_build_type', buildType.lower())
-os.putenv('isam_build_param', buildParam.lower())    
-os.putenv('isam_operating_system', operatingSystem.lower())
-    
-# Start to build parts of the system
-if buildType.upper() == "THIRDPARTY":
-    if os.system('python ' + cwd + '/buildtools/build_third_party.py') != EX_OK:
-        sys.exit(1)
-elif buildType.upper() == "CLEAN":
-    print '-> Cleaning previous build information'
-    os.system('rm -rf build/win32/debug')    
-    os.system('rm -rf build/win32/release')    
-    os.system('rm -rf build/linux/debug')    
-    os.system('rm -rf build/linux/release')  
-elif buildType.upper() == "CLEANALL":    
-    print '-> Cleaning all previous build information'
-    os.system('rm -rf build/win32/debug')    
-    os.system('rm -rf build/win32/release')  
-    os.system('rm -rf build/win32/thirdparty')
-    os.system('rm -rf build/linux/debug')    
-    os.system('rm -rf build/linux/release')  
-    os.system('rm -rf build/linux/thirdparty')
-else:
-    command = 'python ' + os.path.normpath(cwd + '/buildtools/build_main_code.py')
-    if os.system(command) != EX_OK:
-        sys.exit(1)
-    
-sys.exit()
+  
