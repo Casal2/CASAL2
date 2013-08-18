768d39d82a0a4eea475a4e3d5a267af57287a59f
Scott Rasmussen (Zaita) <scott@zaita.com>
2013-08-19 11:10:33 +1200

    CHG: Finished added the AgeSizes (Schnute and Vonbertalanffy) ADD: Empty unit tests files to hold the tests

---
+++ iSAM/source/AgeSizes/Children/Schnute.Test.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file Schnute.Test.cpp
+ * @author  Scott Rasmussen (scott.rasmussen@zaita.com)
+ * @date 19/08/2013
+ * @section LICENSE
+ *
+ * Copyright NIWA Science ©2013 - www.niwa.co.nz
+ *
+ */
+#ifdef TESTMODE
+
+// Headers
+#include "Schnute.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <boost/lexical_cast.hpp>
+
+// Namespaces
+namespace isam {
+
+using ::testing::Return;
+
+/**
+ * Test the results of our KnifeEdge are correct
+ */
+TEST(AgeSizes, Schnute) {
+//  isam::agesizes::Schnute snute;
+
+}
+
+}
+
+
+#endif
