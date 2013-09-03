ba42534d68cfd574063663ecb49dcfd02c783094
Scott Rasmussen (Zaita) <scott@zaita.com>
2013-09-03 15:21:51 +1200

    CHG: Working on BH Recruitment unit tests

--- iSAM/source/Processes/Children/RecruitmentBevertonHolt.Test.cpp
+++ iSAM/source/Processes/Children/RecruitmentBevertonHolt.Test.cpp
@@ -18,6 +18,7 @@
 #include "TimeSteps/Factory.h"
 #include "TimeSteps/Manager.h"
 #include "Partition/Partition.h"
+#include "Partition/Accessors/All.h"
 #include "TestResources/TestFixtures/InternalEmptyModel.h"
 #include "TestResources/ConfigurationFiles/AgeSizes.h"
 #include "TestResources/ConfigurationFiles/Categories.h"
@@ -58,7 +59,7 @@ TEST_F(InternalEmptyModel, Processes_BevertonHolt_Recruitment) {
   AddConfigurationLine(selectivities_constant_one, __FILE__, __LINE__);
   AddConfigurationLine(selectivities_logistic_producing_maturation, __FILE__, __LINE__);
   AddConfigurationLine(size_weights_none, __FILE__, __LINE__);
-  AddConfigurationLine("@time_step time_step_one; processes ageing recruitment maturation mortality", __FILE__, __LINE__);
+  AddConfigurationLine("@time_step time_step_one; processes ageing maturation mortality", __FILE__, __LINE__);
 
   LoadConfiguration();
 
@@ -66,51 +67,14 @@ TEST_F(InternalEmptyModel, Processes_BevertonHolt_Recruitment) {
   ModelPtr model = Model::Instance();
   model->Start();
 
-//
-//
-//  isam::ProcessPtr process = processes::Factory::Create(PARAM_RECRUITMENT, PARAM_BEVERTON_HOLT);
-//  process->parameters().Add(PARAM_LABEL, PARAM_RECRUITMENT, __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_TYPE, PARAM_BEVERTON_HOLT, __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_CATEGORIES, "immature", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_PROPORTIONS, "1.0", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_R0, "100", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_AGE, "1", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_SSB, "ssb", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_B0, "initialisation_phase_one", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_SSB_OFFSET, "1", __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_STANDARDISE_YCS_YEARS, "1994-2006"
+  partition::Category& immature_male   = Partition::Instance().category("immature");
+  partition::Category& immature_female = Partition::Instance().category("mature");
 
-//  process->parameters().Add(PARAM_AGE, "1", __FILE__, __LINE__);
-//
-//  isam::base::ObjectPtr time_step = timesteps::Factory::Create();
-//  time_step->parameters().Add(PARAM_LABEL, "step_one", __FILE__, __LINE__);
-//  time_step->parameters().Add(PARAM_PROCESSES, "recruitment", __FILE__, __LINE__);
-//
-//  Model::Instance()->Start();
-//
-//  partition::Category& immature_male   = Partition::Instance().category("immature.male");
-//  partition::Category& immature_female = Partition::Instance().category("immature.female");
-//
-//  // Verify blank partition
-//  for (unsigned i = 0; i < immature_male.data_.size(); ++i)
-//    EXPECT_DOUBLE_EQ(0.0, immature_male.data_[i]) << " where i = " << i << "; age = " << i + immature_male.min_age_;
-//  for (unsigned i = 0; i < immature_female.data_.size(); ++i)
-//    EXPECT_DOUBLE_EQ(0.0, immature_female.data_[i]) << " where i = " << i << "; age = " << i + immature_female.min_age_;
-//
-//
-//  /**
-//   * Do 1 iteration of the model then check the categories to see if
-//   * the recruitment was successful
-//   */
-//  Model::Instance()->FullIteration();
-//
-//  EXPECT_DOUBLE_EQ(900000, immature_male.data_[0]);
-//  EXPECT_DOUBLE_EQ(600000, immature_female.data_[0]);
-//
-//  for (unsigned i = 1; i < immature_male.data_.size(); ++i)
-//    EXPECT_DOUBLE_EQ(0.0, immature_male.data_[i]) << " where i = " << i << "; age = " << i + immature_male.min_age_;
-//  for (unsigned i = 1; i < immature_female.data_.size(); ++i)
-//    EXPECT_DOUBLE_EQ(0.0, immature_female.data_[i]) << " where i = " << i << "; age = " << i + immature_female.min_age_;
+  // Verify blank partition
+  for (unsigned i = 0; i < immature_male.data_.size(); ++i)
+    EXPECT_DOUBLE_EQ(0.0, immature_male.data_[i]) << " where i = " << i << "; age = " << i + immature_male.min_age_;
+  for (unsigned i = 0; i < immature_female.data_.size(); ++i)
+    EXPECT_DOUBLE_EQ(0.0, immature_female.data_[i]) << " where i = " << i << "; age = " << i + immature_female.min_age_;
 }
 
 } /* namespace processes */
