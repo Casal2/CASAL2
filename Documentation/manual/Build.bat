960fa00fa6838bfe22c895a4209c89d0d4f478ea
alistairdunn1 <alistairdunn1@gmail.com>
2014-06-12 15:16:55 +1200

    FIX: Initial configuration of syntax files for the manual

---
+++ Documentation/manual/Build.bat
@@ -0,0 +1,15 @@
+svn_version --path ..\ --format tex --recursive --quiet --suffix Doc
+"../../isam" -v > iSAMversion.tex
+rem python QuickReference.py
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe" SAM
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\bibtex.exe" SAM
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\makeindex.exe" SAM
+rem python QuickReference.py
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe" SAM
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\bibtex.exe" SAM
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\makeindex.exe" SAM
+rem python QuickReference.py
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe" SAM
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\bibtex.exe" SAM
+"C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\makeindex.exe" SAM
+
