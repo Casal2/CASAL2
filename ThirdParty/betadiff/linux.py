f3d394a8105d48ba8e5485ff34e60cc8658f21ad
Scott Rasmussen (Zaita) <scott@zaita.com>
2014-07-03 14:26:07 +1200

    ADD: Linux build script for BetaDiff

---
+++ ThirdParty/betadiff/linux.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+from distutils import dir_util
+
+import Globals
+
+def doBuild():
+  # Variables
+  fileName         = 'betadiff'
+  targetPath       = Globals.target_path_
+  operatingSystem  = Globals.operating_system_
+  
+  # Clean our any existing files if they already exist
+  print '-- Cleaning files'
+  if os.path.exists(fileName):
+    shutil.rmtree(fileName)
+      
+  # Decompress our archive
+  print '-- Decompressing - check isam_unzip.log'
+  if os.path.exists(fileName + '.zip'):
+    os.system('unzip ' + fileName + '.zip 1> isam_unzip.log 2>&1')
+
+  # Run CMake
+  print '-- Configuring cmake - check ' + fileName + '/isam_cmake.log'
+  os.chdir(fileName)
+  os.system('cmake -G "Unix Makefiles" 1> isam_cmake.log 2>&1')
+
+  # Build 
+  print '-- Building - check ' + fileName + '/isam_build.log'
+  os.system("make 1> isam_build.log 2>&1")
+ 
+  # Move our headers and libraries
+  print '-- Moving headers and libraries'
+  shutil.copy('libbetadiff.a', targetPath + "/lib/")
+  
+  # Finally touch the success file to create it
+  os.system('touch ' + targetPath + '/betadiff.success') 
+  return True
