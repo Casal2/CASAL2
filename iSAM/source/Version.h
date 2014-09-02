fabee1655f89d221859c17a775d6f581cb9caea5
Scott Rasmussen (Zaita) <scott@zaita.com>
2014-09-02 14:15:34 +1200

    CHG: change app name referencing from SAM/sam to iSAM/isam FIX: Build system git call wasn't working as expected on Linux

--- iSAM/source/Version.h
+++ iSAM/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 624dd6ca2920a3634a87394420d3e190abc53d17
-#define SOURCE_CONTROL_DATE "2014-09-01"
+#define SOURCE_CONTROL_REVISION fdfb4613c8d60ecf1f0eb3a7d7ec1e019b3f83b3
+#define SOURCE_CONTROL_DATE "2014-09-02"
 #define SOURCE_CONTROL_YEAR "2014"
 #define SOURCE_CONTROL_MONTH "September"
-#define SOURCE_CONTROL_TIME "23:26:50"
-#define SOURCE_CONTROL_VERSION "2014-09-01 23:26:50 UTC (rev. 624dd6c)"
+#define SOURCE_CONTROL_TIME "01:50:03"
+#define SOURCE_CONTROL_VERSION "2014-09-02 01:50:03 UTC (rev. fdfb461)"
 #endif
