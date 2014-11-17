76229a02a08eefdca182b1ef7a8823e563e97a4f
Scott Rasmussen (Zaita) <scott@zaita.com>
2014-11-17 15:40:44 +1300

    ADD: Windows build for new third party ADOLC library

---
+++ ThirdParty/adolc/windows.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+import time
+from distutils import dir_util
+
+import Globals
+
+class Builder:
+  version_ = 2.5
+    
+  def start(self):
+    # Variables
+    fileName         = 'msys'    
+    operatingSystem  = Globals.operating_system_
+    
+    headers  = 'adolc'
+    library = 'libadolc.a'
+    
+    # Clean our any existing files if they already exist
+    print '-- Cleaning files'  
+    if os.path.exists(fileName):
+      os.system('chmod 777 -R ' + fileName)
+      shutil.rmtree(fileName)
+
+    print '-- Removing previously built headers and libraries'
+    os.system('rm -rf ' + Globals.target_include_path_ + headers)
+    os.system('rm -rf ' + Globals.target_debug_lib_path_ + library)
+    os.system('rm -rf ' + Globals.target_release_lib_path_ + library)        
+        
+    # Decompress our archive
+    print '-- Decompressing - check isam_unzip.log'
+    if os.path.exists(fileName + '.zip'):
+      os.system('unzip ' + fileName + '.zip 1> isam_unzip.log 2>&1')
+    
+    # Build
+    os.chdir(fileName + '\\1.0\\')
+    print '-- Starting msys to decompress and build the ADOLC libary'
+    os.system("msys.bat --norxvt")
+
+    count = 0
+    while not os.path.exists('home/Admin/ADOL-C-2.5.2/ADOL-C/.libs/libadolc.a'):
+      count += 1
+      if count > 60:
+        print '-- Timeout exceeded while waiting for msys to compile ADOL-C'
+        return False
+      if count % 6 == 0:
+        print '-- Waiting for msys compilation to finish'
+      time.sleep(5);
+   
+    # Move our headers and libraries
+    print '-- Moving headers and libraries'
+    os.chdir('home/Admin/ADOL-C-2.5.2/ADOL-C/')
+    dir_util.copy_tree('include/' + headers, Globals.target_include_path_ + '/adolc/')
+    shutil.copy('.libs/' + library, Globals.target_debug_lib_path_)
+    shutil.copy('.libs/' + library, Globals.target_release_lib_path_)    
+     
+    return True
