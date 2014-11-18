7edace69b2de2706ff55156b711665b98553f9aa
Scott Rasmussen (Zaita) <scott@zaita.com>
2014-11-18 16:23:43 +1300

    CHG: BetaDiff now compiles ADD: Linux build file for ADOLC

--- ThirdParty/betadiff/windows.py
+++ ThirdParty/betadiff/windows.py
@@ -40,6 +40,7 @@ class Builder:
    
     # Move our headers and libraries
     print '-- Moving headers and libraries'
+    dir_util.copy_tree('ADOLC18/', Globals.target_include_path_ + '/ADOLC18/')
     shutil.copy(header, Globals.target_include_path_)
     shutil.copy(library, Globals.target_debug_lib_path_)
     shutil.copy(library, Globals.target_release_lib_path_)    
