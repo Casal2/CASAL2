9e60ee754cd812fa006172ea7a71573098a299a6
nobody <nobody@Osiris.int>
2014-12-03 13:21:09 +1300

    CHG: Linux build for cppad has been developed

--- ThirdParty/cppad/linux.py
+++ ThirdParty/cppad/linux.py
@@ -1,47 +1,89 @@
 import os
 import shutil
+import time
 from distutils import dir_util
 
 import Globals
 
+EX_OK = getattr(os, "EX_OK", 0)
+
 class Builder:
-  version_ = 2.52
-  
-  def start(self):     
-    # Variables
-    fileName         = 'ADOL-C-2.5.2'    
-    operatingSystem  = Globals.operating_system_
+  version_ = 3.11
     
-    headers  = 'adolc'
-    library = 'libadolc.a'
+  def start(self):
+    # Variables
+    ipOptFileName         = 'Ipopt-3.11.9'
+    cppadFileName         = 'cppad-20141128.epl'
+    cppadShortFileName    = 'cppad-20141128'
+    libraries = [ "libcoinblas.a", "libcoinmetis.a", "libcoinmumps.a", "libcoinlapack.a", "libipopt.a" ]
     
     # Clean our any existing files if they already exist
-    print '-- Cleaning files'  
-    if os.path.exists(fileName):
-      os.system('chmod 777 -R ' + fileName)
-      shutil.rmtree(fileName)
+    print '-- Cleaning files'
+    if os.path.exists(ipOptFileName):
+      os.system('chmod 777 -R ' + ipOptFileName)
+      shutil.rmtree(ipOptFileName)    
+    if os.path.exists(cppadFileName):
+      os.system('chmod 777 -R ' + cppadFileName)
+      shutil.rmtree(cppadFileName)
+    if os.path.exists(cppadShortFileName):
+      os.system('chmod 777 -R ' + cppadShortFileName)
+      shutil.rmtree(cppadShortFileName)
+    os.system('rm -rf *.tar')
 
     print '-- Removing previously built headers and libraries'
-    os.system('rm -rf ' + Globals.target_include_path_ + headers)
-    os.system('rm -rf ' + Globals.target_special_lib_path_ + library)
-    
+    os.system('rm -rf ' + Globals.target_include_path_ + "cppad")
+    os.system('rm -rf ' + Globals.target_include_path_ + "coin")
+    for library in libraries:
+      os.system('rm -rf ' + Globals.target_special_lib_path_ + library)        
+        
     # Decompress our archive
     print '-- Decompressing - check isam_unzip.log'
-    if os.path.exists(fileName + '.zip'):
-      os.system('unzip ' + fileName + '.zip 1> isam_unzip.log 2>&1')
-
+    if os.path.exists(ipOptFileName + '.tgz'):
+      os.system('gzip -dk ' + ipOptFileName + '.tgz 1> isam_unzip.log 2>&1')
+      os.system('tar xvf ' + ipOptFileName + '.tar 1> isam_untar.log 2>&1')
+    if os.path.exists(cppadFileName + '.tgz'):
+      os.system('gzip -dk ' + cppadFileName + '.tgz 1> isam_unzip.log 2>&1')
+      os.system('tar xvf ' + cppadFileName + '.tar 1> isam_untar.log 2>&1')
+    
     # Build
-    os.chdir(fileName)
-    print '-- Configuring - check isam_configure.log'
-    os.system("./configure 1> isam_configure.log 2>&1")
-    print '-- Building - check isam_build.log'
-    os.system("make 1> isam_build.log 2>&1")
+    os.chdir(ipOptFileName + '/ThirdParty/ASL')      
+    print '-- Building IpOPT Library'
+    if os.system('./get.ASL 1> isam_get.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to download third party library. Please see isam_get.log for error")
+    os.chdir('../Blas')
+    if os.system('./get.Blas 1> isam_get.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to download third party library. Please see isam_get.log for error")
+    os.chdir('../Lapack')
+    if os.system('./get.Lapack 1> isam_get.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to download third party library. Please see isam_get.log for error")
+    os.chdir('../Metis')
+    if os.system('./get.Metis 1> isam_get.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to download third party library. Please see isam_get.log for error")
+    os.chdir('../Mumps')
+    if os.system('./get.Mumps 1> isam_get.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to download third party library. Please see isam_get.log for error")
+    os.chdir('../../')
+    if os.system('./configure --enable-static --disable-shared 1> isam_configure.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to configure code base. Please see above for build error")
+    if os.system('make 1> isam_make.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to build code base. Please see above for build error")
+    if os.system('make install 1> isam_make_install.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to install code base. Please see above for build error")
+    os.chdir('../')
 
+    print '-- Building CppAD Library'
+    os.chdir(cppadShortFileName)
+    if os.system('./configure --prefix=$PWD 1> isam_configure.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to configure code base. Please see above for build error")                      
+    if os.system('make install 1> make_install.log 2>&1') != EX_OK:
+      return Globals.PrintError("Failed to install code base. Please see above for build error")    
+    os.chdir('../')
+    
     # Move our headers and libraries
     print '-- Moving headers and libraries'
-    os.chdir('ADOL-C/')
-    dir_util.copy_tree('include/' + headers, Globals.target_include_path_ + '/adolc/')
-    shutil.copy('.libs/' + library, Globals.target_special_lib_path_)
-
+    dir_util.copy_tree(cppadShortFileName + '/include/cppad', Globals.target_include_path_ + '/cppad/')
+    dir_util.copy_tree(ipOptFileName + '/include/coin', Globals.target_include_path_ + '/coin/')
+    for library in libraries:
+      shutil.copy(ipOptFileName + '/lib64/' + library, Globals.target_special_lib_path_)    
+     
     return True
-    
