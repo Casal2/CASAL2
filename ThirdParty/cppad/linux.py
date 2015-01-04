3e8d0f36aa5c19130407cf930585f0e5620daab2
Scott Rasmussen (Zaita) <scott@zaita.com>
2015-01-05 10:52:58 +1300

    CHG: Added missing method to CPPAD for Linux

--- ThirdParty/cppad/linux.py
+++ ThirdParty/cppad/linux.py
@@ -13,8 +13,7 @@ class Builder:
   def start(self):
     # Variables
     ipOptFileName         = 'Ipopt-3.11.9'
-    cppadFileName         = 'cppad-20141128.epl'
-    cppadShortFileName    = 'cppad-20141128'
+    cppadFileName         = 'cppad-20141128'
     libraries = [ "libcoinblas.a", "libcoinmetis.a", "libcoinmumps.a", "libcoinlapack.a", "libipopt.a" ]
     
     # Clean our any existing files if they already exist
@@ -25,9 +24,6 @@ class Builder:
     if os.path.exists(cppadFileName):
       os.system('chmod 777 -R ' + cppadFileName)
       shutil.rmtree(cppadFileName)
-    if os.path.exists(cppadShortFileName):
-      os.system('chmod 777 -R ' + cppadShortFileName)
-      shutil.rmtree(cppadShortFileName)
     os.system('rm -rf *.tar')
 
     print '-- Removing previously built headers and libraries'
@@ -41,9 +37,8 @@ class Builder:
     if os.path.exists(ipOptFileName + '.tgz'):
       os.system('gzip -dk ' + ipOptFileName + '.tgz 1> isam_unzip.log 2>&1')
       os.system('tar xvf ' + ipOptFileName + '.tar 1> isam_untar.log 2>&1')
-    if os.path.exists(cppadFileName + '.tgz'):
-      os.system('gzip -dk ' + cppadFileName + '.tgz 1> isam_unzip.log 2>&1')
-      os.system('tar xvf ' + cppadFileName + '.tar 1> isam_untar.log 2>&1')
+    if os.path.exists(cppadFileName + '.zip'):
+      os.system('unzip ' + cppadFileName + '.zip 1> isam_unzip.log 2>&1')
     
     # Build
     os.chdir(ipOptFileName + '/ThirdParty/ASL')      
@@ -72,7 +67,7 @@ class Builder:
     os.chdir('../')
 
     print '-- Building CppAD Library'
-    os.chdir(cppadShortFileName)
+    os.chdir(cppadFileName)
     if os.system('./configure --prefix=$PWD 1> isam_configure.log 2>&1') != EX_OK:
       return Globals.PrintError("Failed to configure code base. Please see above for build error")                      
     if os.system('make install 1> make_install.log 2>&1') != EX_OK:
@@ -81,7 +76,7 @@ class Builder:
     
     # Move our headers and libraries
     print '-- Moving headers and libraries'
-    dir_util.copy_tree(cppadShortFileName + '/include/cppad', Globals.target_include_path_ + '/cppad/')
+    dir_util.copy_tree(cppadFileName + '/include/cppad', Globals.target_include_path_ + '/cppad/')
     dir_util.copy_tree(ipOptFileName + '/include/coin', Globals.target_include_path_ + '/coin/')
     if os.path.exists(ipOptFileName + '/lib64'):
       for library in libraries:
