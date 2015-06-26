2f45169c8e90e82a3c1a904fc733f5c6cbf85ede
Scott Rasmussen (Zaita) <scott@zaita.com>
2015-06-26 16:06:10 +1200

    FIX: Add error_value_multiplier to Multinomial GetInitialScore()

--- iSAM/source/Likelihoods/Children/Multinomial.cpp
+++ iSAM/source/Likelihoods/Children/Multinomial.cpp
@@ -71,12 +71,12 @@ Double Multinomial::GetInitialScore(map<unsigned, vector<observations::Compariso
       continue;
 
     last_category = comparison.category_;
-    Double temp_score = -math::LnFactorial(AdjustErrorValue(comparison.process_error_, comparison.error_value_));
-    LOG_FINEST() << "Adding: " << temp_score << " = LnFactorial(AdjustErrorValue(" << comparison.process_error_ << ", " << comparison.error_value_ << ")";
+    Double temp_score = -math::LnFactorial(AdjustErrorValue(comparison.process_error_, comparison.error_value_)  * error_value_multiplier_);
+    LOG_FINEST() << "Adding: " << temp_score << " = LnFactorial(AdjustErrorValue(" << comparison.process_error_ << ", " << comparison.error_value_ << ")  * " << error_value_multiplier_ << ")";
     score += temp_score;
   }
 
-  return score;
+  return score * multiplier_;
 }
 
 /**
