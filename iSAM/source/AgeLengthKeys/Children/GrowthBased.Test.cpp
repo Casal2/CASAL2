a8b2876dfe099521c78c5f6c1af43fccb2342134
Craig44 <Craig.Marsh@niwa.co.nz>
2015-07-27 15:30:14 +1200

    ADD: AgeLengthKeys/Children/GrowthBased.Test.cpp

---
+++ iSAM/source/AgeLengthKeys/Children/GrowthBased.Test.cpp
@@ -0,0 +1,59 @@
+//============================================================================
+// Name        : GrowthBased.Test.cpp
+// Copyright   : Copyright NIWA Science ©2009 - www.niwa.co.nz
+//============================================================================
+#ifdef TESTMODE
+
+// Headers
+#include "GrowthBased.h"
+
+#include <iostream>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "AgeLengthKeys/Factory.h"
+#include "Utilities/RandomNumberGenerator.h"
+
+// Namespaces
+namespace niwa {
+namespace AgeLengthKeys {
+
+using std::cout;
+using std::endl;
+
+// classes
+class GrowthBasedTest {
+public:
+//Double Cumm(
+//		Double          mu,
+//		Double          cv,
+//		vector<Double>  *vprop_in_length,
+//		vector<Double>  class_mins,
+//		string          distribution,
+//		vector<Double>  Class_min_temp,
+//		bool 			plus_grp
+//		)
+//		{
+//
+//	 GrowthBased x;
+//	 x.CummulativeNormal(mu, cv);
+//  }
+};
+
+//TEST(AgeLengthKeys, CummulativeNormal_1) {
+//	Double          mu = 35.49858,
+//	Double          cv = 3.549858,
+//	vector<Double>  vprop_in_length,
+//	vector<Double>  class_mins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47},
+//	string          distribution = "normal",
+//	vector<Double>  Class_min_temp,
+//	bool 			plus_grp = 1
+//
+//	GrowthBasedTest myTest;
+//	myTest.Cumm(mu, cv, vprop_in_length, class_mins,distribution,Class_min_temp,plus_grp)
+//}
+
+}
+}
+
+#endif
