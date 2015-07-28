bfa77ac46878af7490cf898c538b3a6d32e8fb94
Craig44 <Craig.Marsh@niwa.co.nz>
2015-07-28 16:10:33 +1200

    CHG: GrowthBased.cpp CHG: GrowthBased.Test.cpp

--- iSAM/source/AgeLengthKeys/Children/GrowthBased.Test.cpp
+++ iSAM/source/AgeLengthKeys/Children/GrowthBased.Test.cpp
@@ -14,49 +14,75 @@
 #include "AgeLengthKeys/Children/GrowthBased.h"
 #include "Utilities/RandomNumberGenerator.h"
 
+
+
 // Namespaces
 namespace niwa {
-namespace agelengthkeys {
+  namespace agelengthkeys {
 
-using std::cout;
-using std::endl;
+    using std::cout;
+    using std::endl;
 
 // classes
-class GrowthBasedTest {
-public:
-  void CummulativeNormal(Double mu, Double cv, vector<Double> *vprop_in_length, vector<Double> class_mins, string distribution, vector<Double>  Class_min_temp, bool plus_grp) {
-    GrowthBased x;
-    x.CummulativeNormal(mu, cv, vprop_in_length, class_mins, distribution, Class_min_temp, plus_grp);
-  }
-};
+    class GrowthBasedTest {
+    public:
+      void CummulativeNormal(Double mu, Double cv, vector<Double> *vprop_in_length, vector<Double> class_mins, string distribution, vector<Double> Class_min_temp, bool plus_grp) {
+        GrowthBased x;
+        x.CummulativeNormal(mu, cv, vprop_in_length, class_mins, distribution, Class_min_temp, plus_grp);
+      }
+    };
 
-/**
- * Test
- */
-TEST(AgeLengthKeys, CummulativeNormal_1) {
-	Double          mu = 35.49858;
-	Double          cv = 3.549858;
-	vector<Double>  vprop_in_length;
-	vector<Double>  class_mins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
-	string          distribution = "normal";
-	vector<Double>  Class_min_temp = {};
-	bool 			      plus_grp = 1;
+    /**
+     * Test
+     */
+    TEST(AgeLengthKeys, CummulativeNormal_1) {
+      Double mu = 35.49858;
+      Double cv = 3.549858;
+      vector<Double> vprop_in_length;
+      vector<Double> class_mins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+      string distribution = "normal";
+      vector<Double> Class_min_temp = {};
+      bool plus_grp = 1;
 
-	GrowthBasedTest myTest;
-	myTest.CummulativeNormal(mu, cv, &vprop_in_length, class_mins, distribution, Class_min_temp, plus_grp);
+      GrowthBasedTest myTest;
+      myTest.CummulativeNormal(mu, cv, &vprop_in_length, class_mins, distribution, Class_min_temp, plus_grp);
 
- vector<Double> expected = {3.871360e-09, 1.596024e-08, 7.422371e-08, 3.190201e-07, 1.267264e-06, 4.652548e-06, 1.578663e-05, 4.950653e-05, 1.434857e-04, 3.843497e-04,9.515104e-04,
- 2.177043e-03, 4.603455e-03, 8.996259e-03, 1.624797e-02, 2.712032e-02, 4.183597e-02, 5.964394e-02, 7.858619e-02, 9.569515e-02,
- 1.076949e-01, 1.120122e-01, 1.076708e-01, 9.565224e-02, 7.853334e-02, 5.959046e-02, 4.178909e-02, 2.708385e-02, 1.622248e-02, 8.980133e-03,
- 4.594173e-03, 2.172166e-03, 9.491660e-04, 5.977795e-04};
+      vector<Double> expected = {3.87136e-09, 1.59602e-008, 7.42236e-008, 3.1902e-007, 1.26726e-006, 4.65254e-006, 1.57866e-005, 4.95064e-005, 0.000143486, 0.000384349,0.000951509,
+        0.00217704, 0.00460345, 0.00899625, 0.016248, 0.0271203, 0.0418359, 0.0596439, 0.0785861, 0.0956951,
+        0.107695, 0.112012, 0.107671, 0.0956523, 0.0785334, 0.0595905, 0.0417891, 0.0270839, 0.0162225, 0.00898015,
+        0.00459418, 0.00217217, 0.000949168, 0.000597781};
 
- ASSERT_EQ(expected.size(), vprop_in_length.size());
- for (unsigned i = 0; i < expected.size(); ++i) {
-   EXPECT_EQ(expected[i], vprop_in_length[i]) << " with i = " << i;
- }
-}
+      ASSERT_EQ(expected.size(), vprop_in_length.size());
+      for (unsigned i = 0; i < expected.size(); ++i) {
+        EXPECT_EQ(expected[i], vprop_in_length[i]) << " with i = " << i;
+      }
+    }
 
-} /* namespace agelengthkeys */
+    TEST(AgeLengthKeys, CummulativeNormal_2) {
+      Double mu = 35.49858;
+      Double cv = 3.549858;
+      vector<Double> vprop_in_length;
+      vector<Double> class_mins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+      string distribution = "lognormal";
+      vector<Double> Class_min_temp = {};
+      bool plus_grp = 1;
+
+      GrowthBasedTest myTest;
+      myTest.CummulativeNormal(mu, cv, &vprop_in_length, class_mins, distribution, Class_min_temp, plus_grp);
+
+      vector<Double> expected = {0, 9.99201e-016, 1.13909e-013, 6.90781e-012, 2.48631e-010, 5.68087e-009, 8.71919e-008, 9.42695e-007, 7.47451e-006, 4.49824e-005, 0.000211637, 0.000798628, 0.00247155, 0.00639629,0.0140816,
+        0.0267735, 0.0445555, 0.0656767, 0.0866652, 0.103353, 0.112342, 0.112157, 0.103555, 0.088979, 0.0715524, 0.0541266, 0.0386965, 0.0262575, 0.0169761, 0.0104946, 0.00622371, 0.0035513,0.00195511, 0.00209553};
+
+      ASSERT_EQ(expected.size(), vprop_in_length.size());
+      for (unsigned i = 0; i < expected.size(); ++i) {
+        EXPECT_EQ(expected[i], vprop_in_length[i]) << " with i = " << i;
+      }
+    }
+
+
+
+
+  } /* namespace agelengthkeys */
 } /* namespace niwa */
 
 #endif
--- iSAM/source/AgeLengthKeys/Children/GrowthBased.cpp
+++ iSAM/source/AgeLengthKeys/Children/GrowthBased.cpp
@@ -35,12 +35,14 @@ namespace agelengthkeys {
  * Note: The constructor is parsed to generate Latex for the documentation.
  */
 GrowthBased::GrowthBased() {
-  parameters_.Bind<Double>(PARAM_CLASS_MINIMUMS, &class_mins_, "Length Class Minimums", "")->set_lower_bound(0.0, false);
-  parameters_.Bind<string>(PARAM_DISTRIBUTION, &distribution_, "Distribution around each Age length mean","")->set_allowed_values({ PARAM_NORMAL, PARAM_LOGNORMAL });
+  parameters_.Bind<Double>(PARAM_CLASS_MINIMUMS, &class_mins_,
+      "Length Class Minimums", "")->set_lower_bound(0.0, false);
+  parameters_.Bind<string>(PARAM_DISTRIBUTION, &distribution_,
+      "Distribution around each Age length mean", "")->set_allowed_values( {
+      PARAM_NORMAL, PARAM_LOGNORMAL });
 }
 
-
-/** Calculate a vector of length frequencies based on a distribution
+/** Estimates proportion of age class that is in each length interval
  * @parm mu , cv = from @age_length
  * @parm *vprop_in_length   = is the output vector that is changed by reference
  * @parm class_mins  is a vector of length minimums feed in from the config file
@@ -49,73 +51,70 @@ GrowthBased::GrowthBased() {
  *
  */
 void GrowthBased::CummulativeNormal(
-	Double          mu, 		    //mean of length distribution
-	Double          cv, 		    // cv from @age_length
-	vector<Double>  *vprop_in_length,  // fraction of fish in each length class return
-	vector<Double>  class_mins,
-	string          distribution,
-	vector<Double>  Class_min_temp,
-	bool 			plus_grp
-	)
-	{
+    Double mu, 		    //mean of length distribution
+    Double cv, 		    // cv from @age_length
+    vector<Double> *vprop_in_length, // fraction of fish in each length class return
+    vector<Double> class_mins, string distribution,
+    vector<Double> Class_min_temp, bool plus_grp) {
 
-// // est proportion of age class that are in each length interval
-//  Class_min_temp = class_mins; 	// Temp vec is a vector that all the work gets done on.
-//  Double sigma = cv * mu;
-//  if(distribution == "lognormal"){ // if lognormal, convert growth parameters to log space; use class_mins->is_log
-//  Double cv     = sigma / mu;
-//  Double  Lvar  = log(cv*cv + 1.0) ;      //variance in log space
-//  mu    		= log(mu) - Lvar / 2.0 ; //mean in log space
-//  sigma 		= sqrt(Lvar);
-//
-//  if(class_mins[0] < 0.0001) Class_min_temp[0] = log(0.0001) ; else Class_min_temp[0] = log(class_mins[0]);
-//    if(class_mins.size() > 1) for (unsigned h =1; h < class_mins.size() ; h++)   Class_min_temp[h] = log(class_mins[h]);
-//
-//  }
-//
-//
-// Double z, tt, norm, ttt, tmp,CumN_0,  CumN, CumN_prev, sumN ;
-// const Double pi = 3.141592653589793;
-// std::vector<int>::size_type sz = class_mins.size();
-// vprop_in_length->resize(sz);
-//
-// for(unsigned j = 0; j < sz; ++j)
-// {
-//  z = (Class_min_temp[j] - mu) / sigma;
-//  tt = 1.0/(1.0 + 0.2316419 * z);
-//  norm = 1.0/sqrt(2 * pi)  * exp( -0.5*z * z);
-//  ttt  = tt;
-//  tmp  = 0.319381530 * ttt ;
-//  ttt  = ttt * tt;
-//  tmp  = tmp - 0.356563782 * ttt;          // tt^2
-//  ttt  = ttt * tt;
-//  tmp  = tmp + 1.781477937 * ttt ;       // tt^3
-//  ttt  = ttt * tt;
-//  tmp  = tmp - 1.821255978 * ttt;        // tt^4
-//  ttt  = ttt * tt;
-//  tmp  = tmp + 1.330274429 * ttt ;      // tt^5
-//
-//  CumN = 1.0 - norm*tmp;
-//  if (Class_min_temp[j] < mu)	CumN = 1.0 - CumN;
-//
-//
-//  if( (j = 0) ){
-//  CumN_0 = CumN;
-//  CumN_prev = CumN;
-//  } else {
-//
-//  (*vprop_in_length)[j-1] = CumN - CumN_prev;
-//  sumN += (*vprop_in_length)[j - 1];
-// }
-//	}
-// if(plus_grp){
-//     (*vprop_in_length)[sz - 1] = 1.0 - sumN - CumN_0 ;    //plus_gp is what is left on the RHS; also need to account for lengths below the first boundary
-//     } else vprop_in_length->resize(sz - 1);			   // remove last array member as it is not needed
+  // est proportion of age class that are in each length interval
+  Class_min_temp = class_mins; // Temp vec is a vector that all the work gets done on.
+  Double sigma = cv; //* mu;
+  if (distribution == "lognormal") { // if lognormal, convert growth parameters to log space
+    // Put parameters in to log space
+    Double cv = sigma / mu;
+    Double Lvar = log(cv * cv + 1.0);
+    mu = log(mu) - Lvar / 2.0;
+    sigma = sqrt(Lvar);
 
- return ;
- }
+    if (class_mins[0] < 0.0001)
+      Class_min_temp[0] = log(0.0001);
+    else
+      Class_min_temp[0] = log(class_mins[0]);
+    if (class_mins.size() > 1)
+      for (unsigned h = 1; h < class_mins.size(); h++)
+        Class_min_temp[h] = log(class_mins[h]);
+
+  }
 
 
+  Double z, tt, norm, ttt, tmp, sum;
+  vector<Double> cum;
+  const Double kPi = 3.141592653589793;
+  std::vector<int>::size_type sz = class_mins.size();
+  vprop_in_length->resize(sz);
+
+  for (unsigned j = 0; j < sz; ++j) {
+    z = fabs((Class_min_temp[j] - mu)) / sigma;
+    tt = 1.0 / (1.0 + 0.2316419 * z);
+    norm = 1.0 / sqrt(2.0 * kPi) * exp(-0.5 * z * z);
+    ttt = tt;
+    tmp = 0.319381530 * ttt;
+    ttt = ttt * tt;
+    tmp = tmp - 0.356563782 * ttt;          // tt^2
+    ttt = ttt * tt;
+    tmp = tmp + 1.781477937 * ttt;       // tt^3
+    ttt = ttt * tt;
+    tmp = tmp - 1.821255978 * ttt;        // tt^4
+    ttt = ttt * tt;
+    tmp = tmp + 1.330274429 * ttt;      // tt^5
+
+    cum.push_back(1.0 - norm * tmp); //Crashing
+    if (Class_min_temp[j] < mu) {
+      cum[j] = 1.0 - cum[j];
+    }
+    if (j > 0) {
+      (*vprop_in_length)[j - 1] = cum[j] - cum[j - 1];
+      sum += (*vprop_in_length)[j - 1];
+    }
+  }
+  if (plus_grp) {
+    (*vprop_in_length)[sz - 1] = 1.0 - sum - cum[0];
+  } else
+    vprop_in_length->resize(sz - 1);
+
+  return;
+}
 
 /**
  * Do the conversion of the partition structure from age to length
@@ -123,32 +122,29 @@ void GrowthBased::CummulativeNormal(
  * @param category The current category to convert
  */
 
-void GrowthBased::DoAgeToLengthConversion(std::shared_ptr<partition::Category> category ) {
-
-//  unsigned min_a = Model::Instance()->min_age();
-//  unsigned max_a = Model::Instance()->max_age();
+void GrowthBased::DoAgeToLengthConversion(
+    std::shared_ptr<partition::Category> category) {
 //
+//  unsigned min_a = category->min_age_;
+//  unsigned max_a = category->max_age_;
 //
-//  for(unsigned age = min_a; age < max_a ; ++age){
+//  for (unsigned age = min_a; age < max_a; ++age) {
 //
-//  category->UpdateMeanLengthData();
+//    category->UpdateMeanLengthData();
 //
+//    Double cv = category->age_length()->cv();
 //
-//  Double cv = category->age_length()->cv();
+//    //
+//    bool plus_grp = Model::Instance()->age_plus();
+//    CummulativeNormal(category->length_per_[age], cv, &Age_freq_, class_mins_, distribution_, Temp_vec, plus_grp);
 //
-//  //
-//  bool plus_grp = Model::Instance()->age_plus();
-//  CummulativeNormal(category->length_per_[age], cv, &Age_freq_ , class_mins_ , distribution_, Temp_vec, plus_grp);
-//
-//  // Loop through the length bins and multiple the partition of the current age to go from
-//  // length frequencies to age length numbers
-//  for(unsigned i = 0; i < class_mins_.size(); ++i){
-//  category->length_data_[age][i] =  category->data_[age] * Age_freq_[i];
-//
-//
+//    // Loop through the length bins and multiple the partition of the current age to go from
+//    // length frequencies to age length numbers
+//    for (unsigned i = 0; i < class_mins_.size(); ++i) {
+//      category->length_data_[age][i] = category->data_[age] * Age_freq_[i];
+//    }
 //  }
-// }
-}
+//}
 
 } /* namespace agelengthkeys */
 } /* namespace niwa */
