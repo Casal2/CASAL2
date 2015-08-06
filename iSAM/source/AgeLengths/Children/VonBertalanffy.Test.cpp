e1e93ef0821474c1f622e6a8f5dd5cf217664242
Craig44 <Craig.Marsh@niwa.co.nz>
2015-08-06 12:23:12 +1200

    CHG: messing around with Von Bert trying to get unit test done but failing

--- iSAM/source/AgeLengths/Children/VonBertalanffy.Test.cpp
+++ iSAM/source/AgeLengths/Children/VonBertalanffy.Test.cpp
@@ -24,16 +24,14 @@ using ::testing::Return;
 
 class VonBertalanffyTest {
 public:
-  void CummulativeNormal(Double mu, Double cv, vector<Double> *vprop_in_length, vector<Double> class_mins, string distribution, bool plus_grp) {
-    VonBertalanffyTest x;
-    x.CummulativeNormal(mu, cv, vprop_in_length, class_mins, distribution, plus_grp);
+  void CummulativeNormal(Double mu, Double cv, vector<Double> *vprop_in_length, vector<Double> length_bins, string distribution, bool plus_grp) {
+    VonBertalanffy x;
+    x.CummulativeNormal(mu, cv, vprop_in_length, length_bins, distribution, plus_grp);
   }
-//  void BuildCV(unsigned year){
-//    VonBertalanffyTest x;
-//    x.BuildCV(year);
-//  }
-//  }
-
+  void BuildCV(unsigned year) {
+    VonBertalanffy x;
+    x.BuildCV(year);
+  }
 };
 
 
@@ -43,14 +41,14 @@ public:
  */
 TEST(AgeLength, VonBertalanffy_CummulativeNormal_1) {
   Double mu = 35.49858;
-  Double cv = 3.549858;
+  Double cv = 0.1;
   vector<Double> vprop_in_length;
-  vector<Double> class_mins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+  vector<Double> length_bins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
   string distribution = "normal";
   bool plus_grp = 1;
 
   VonBertalanffyTest myTest;
-  myTest.CummulativeNormal(mu, cv, &vprop_in_length, class_mins, distribution, plus_grp);
+  myTest.CummulativeNormal(mu, cv, &vprop_in_length, length_bins, distribution,  plus_grp);
 
   vector<Double> expected = {3.8713535710499514e-009, 1.5960216925847703e-008, 7.422358561104403e-008, 3.1901955588331532e-007, 1.2672619864595447e-006, 4.6525401673491729e-006,
       1.5786604316003761e-005, 4.9506445653380027e-005,0.00014348551812060073, 0.00038434913282614502,0.00095150900849361175, 0.0021770396325317964, 0.0046034492460040877,
@@ -69,14 +67,14 @@ TEST(AgeLength, VonBertalanffy_CummulativeNormal_1) {
  */
 TEST(AgeLength, VonBertalanffy_CummulativeNormal_2) {
   Double mu = 35.49858;
-  Double cv = 3.549858;
+  Double cv = 0.1;
   vector<Double> vprop_in_length;
-  vector<Double> class_mins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+  vector<Double> length_bins = {0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ,32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
   string distribution = "lognormal";
   bool plus_grp = 0;
 
   VonBertalanffyTest myTest;
-  myTest.CummulativeNormal(mu, cv, &vprop_in_length, class_mins, distribution, plus_grp);
+  myTest.CummulativeNormal(mu, cv, &vprop_in_length, length_bins, distribution,  plus_grp);
 
   vector<Double> expected = {0, 9.9920072216264089e-016,1.1390888232654106e-013, 6.907807659217724e-012, 2.4863089365112501e-010, 5.6808661108576075e-009, 8.7191919018181352e-008,
       9.4269457673323842e-007, 7.4745056608538363e-006, 4.4982380957292456e-005, 0.00021163731992057677, 0.00079862796125962365, 0.0024715534075264722, 0.0063962867724943751,0.01408161729231916,
@@ -103,7 +101,7 @@ TEST(AgeLength, VonBertalanffy_CummulativeNormal_2) {
 //  process->parameters().Add(PARAM_TYPE, "constant", __FILE__, __LINE__);
 //  process->parameters().Add(PARAM_CATEGORIES, categories, __FILE__, __LINE__);
 //  process->parameters().Add(PARAM_PROPORTIONS, proportions, __FILE__, __LINE__);
-//  process->parameters().Add(PARAM_R0, "1000000", __FILE__, __LINE__);
+//  process->parameters().Add(PARAM_R0, "100000", __FILE__, __LINE__);
 //  process->parameters().Add(PARAM_AGE, "1", __FILE__, __LINE__);
 //
 //  niwa::AgeLengthPtr agelength = agelengths::Factory::Create(PARAM_VON_BERTALANFFY, "");
