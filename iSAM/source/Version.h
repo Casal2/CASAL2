6d30a646a5ecf9ab4a0a4ee4e814654573863d2c
Craig44 <Craig.Marsh@niwa.co.nz>
2015-09-18 15:56:50 +1200

    CHG: MortalityInstantaneous.cpp

--- iSAM/source/Version.h
+++ iSAM/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 6ee4b98ec15e5c8795a6088809e323aff056ecc1
-#define SOURCE_CONTROL_DATE "2015-09-16"
+#define SOURCE_CONTROL_REVISION e66132b7f805543f9b061afc414b4d03a66e5b18
+#define SOURCE_CONTROL_DATE "2015-09-18"
 #define SOURCE_CONTROL_YEAR "2015"
 #define SOURCE_CONTROL_MONTH "September"
-#define SOURCE_CONTROL_TIME "00:08:56"
-#define SOURCE_CONTROL_VERSION "2015-09-16 00:08:56 UTC (rev. 6ee4b98)"
+#define SOURCE_CONTROL_TIME "00:29:01"
+#define SOURCE_CONTROL_VERSION "2015-09-18 00:29:01 UTC (rev. e66132b)"
 #endif
