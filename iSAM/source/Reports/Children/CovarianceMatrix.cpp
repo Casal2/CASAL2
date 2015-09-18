f6d6cee41b5e3dcb741d868c9c5b2e958ebb7e12
Craig44 <Craig.Marsh@niwa.co.nz>
2015-09-18 16:47:18 +1200

    FIX: MortalityInstantaneous.Test.cpp CHG: MCMCChain.cpp and CovarianceMatrix.cpp

--- iSAM/source/Reports/Children/CovarianceMatrix.cpp
+++ iSAM/source/Reports/Children/CovarianceMatrix.cpp
@@ -28,20 +28,37 @@ void CovarianceMatrix::DoBuild() {
 }
 
 void CovarianceMatrix::DoExecute() {
+  /*
+   * This reports the Covariance, Correlation and Hessian matrix
+   */
   LOG_TRACE();
   minimiser_ = minimisers::Manager::Instance().active_minimiser();
   covariance_matrix_ = minimiser_->covariance_matrix();
+  correlation_matrix_ = minimiser_->correlation_matrix();
+  hessian_ = minimiser_->hessian_matrix();
+  unsigned hessian_size  = minimiser_->hessian_size();
 
   cache_ << "*" << label_ << " " << "("<< type_ << ")"<<"\n";
-  cache_ << "MCMC_covariance" << REPORT_R_MATRIX <<"\n";
-  cache_ << " size of covariance matrix ncols = " << covariance_matrix_.size2() << " nrows = " << covariance_matrix_.size1() << "\n";
+  cache_ << "Covariance_Matrix" << REPORT_R_MATRIX <<"\n";
 
   for (unsigned i = 0; i < covariance_matrix_.size1(); ++i) {
-     for (unsigned j = 0; j < covariance_matrix_.size2(); ++j) {
+     for (unsigned j = 0; j < covariance_matrix_.size2(); ++j)
        cache_ << covariance_matrix_ (i,j) << " ";
-       if ( j == (covariance_matrix_.size1() - 1))
-         cache_ << "\n";
-     }
+     cache_ << "\n";
+  }
+
+  cache_ << "*" << "Correlation_Matrix" << REPORT_R_MATRIX <<"\n";
+  for (unsigned i = 0; i < correlation_matrix_.size1(); ++i) {
+    for (unsigned j = 0; j < correlation_matrix_.size2(); ++j)
+      cache_ << correlation_matrix_ (i,j) << " ";
+    cache_ << "\n";
+  }
+
+  cache_ << "*" << "Hessian_Matrix" << REPORT_R_MATRIX <<"\n";
+  for (unsigned i = 0; i < hessian_size; ++i) {
+     for (unsigned j = 0; j < hessian_size; ++j)
+       cache_ << hessian_ [i][j] << " ";
+     cache_ << "\n";
   }
 
   ready_for_writing_ = true;
