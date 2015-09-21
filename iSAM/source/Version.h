44881ac15be2833c6ee7947dcffccac12f289f2c
Craig44 <Craig.Marsh@niwa.co.nz>
2015-09-22 09:30:18 +1200

    ADD: log out

--- iSAM/source/Version.h
+++ iSAM/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION e66132b7f805543f9b061afc414b4d03a66e5b18
-#define SOURCE_CONTROL_DATE "2015-09-18"
+#define SOURCE_CONTROL_REVISION bd4b3bc6bb1443005036e05532d44e627962b9f5
+#define SOURCE_CONTROL_DATE "2015-09-21"
 #define SOURCE_CONTROL_YEAR "2015"
 #define SOURCE_CONTROL_MONTH "September"
-#define SOURCE_CONTROL_TIME "00:29:01"
-#define SOURCE_CONTROL_VERSION "2015-09-18 00:29:01 UTC (rev. e66132b)"
+#define SOURCE_CONTROL_TIME "03:42:06"
+#define SOURCE_CONTROL_VERSION "2015-09-21 03:42:06 UTC (rev. bd4b3bc)"
 #endif
