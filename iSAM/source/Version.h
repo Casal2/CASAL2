2d45d50ee805f2dc94e95b5a49323560d2a87d43
Craig44 <Craig.Marsh@niwa.co.nz>
2015-11-16 14:12:49 +1300

    FIX: MCMC/Children/IndependenceMetropolis.cpp  remove conditions on successful jumps to jumps

--- iSAM/source/Version.h
+++ iSAM/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 302993f8d699df5768c1beb4527e0d1f3ef2f009
-#define SOURCE_CONTROL_DATE "2015-11-11"
+#define SOURCE_CONTROL_REVISION 8636c4ff271a48f1d1bc87d8f7e445239c67ab0d
+#define SOURCE_CONTROL_DATE "2015-11-13"
 #define SOURCE_CONTROL_YEAR "2015"
 #define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "23:40:19"
-#define SOURCE_CONTROL_VERSION "2015-11-11 23:40:19 UTC (rev. 302993f)"
+#define SOURCE_CONTROL_TIME "03:21:13"
+#define SOURCE_CONTROL_VERSION "2015-11-13 03:21:13 UTC (rev. 8636c4f)"
 #endif
