0bec3989d8e53640029a51280c6799537ec3902a
Scott Rasmussen (Zaita) <scott@zaita.com>
2016-01-18 15:56:48 +1300

    ADD: Simple main function for FrontEnd CHG: #ifdef wasn't working properly on Linux ADD: Linux front end code CHG: Added dl library for linking in CMakeLists.txt

--- FrontEnd/source/FrontEnd_Linux.cpp
+++ FrontEnd/source/FrontEnd_Linux.cpp
@@ -1,19 +1,20 @@
 /**
- * @file FrontEnd.cpp
+ * @file FrontEnd_Linux.cpp
  * @author Scott Rasmussen (scott.rasmussen@zaita.com)
  * @github https://github.com/Zaita
- * @date Jan 13, 2016
+ * @date Jan 18, 2016
  * @section LICENSE
  *
  * Copyright NIWA Science �2016 - www.niwa.co.nz
  *
  */
-#ifdef __MINGW32__
+#ifdef __gnu_linux__
 // headers
+#include "FrontEnd.h"
 
 #include <iostream>
 #include <string>
-#include <windows.h>
+#include <dlfcn.h>
 
 #include "Utilities/RunParameters.h"
 
@@ -22,10 +23,11 @@ using std::cout;
 using std::endl;
 using std::string;
 
-typedef int(__cdecl *RUNTESTSPROC)(int, char**);
-typedef int(__cdecl *RUNPROC)(int, char**, niwa::utilities::RunParameters&);
-typedef int(__cdecl *PRELOADPROC)(niwa::utilities::RunParameters&);
-typedef int(__cdecl *LOADOPTIONSPROC)(int, char**, niwa::utilities::RunParameters&);
+
+typedef int(*RUNTESTSPROC)(int, char**);
+typedef int(*RUNPROC)(int, char**, niwa::utilities::RunParameters&);
+typedef int(*PRELOADPROC)(niwa::utilities::RunParameters&);
+typedef int(*LOADOPTIONSPROC)(int, char**, niwa::utilities::RunParameters&);
 
 const string release_dll = "casal2_release.dll";
 const string adolc_dll   = "casal2_adolc.dll";
@@ -37,13 +39,13 @@ const string test_dll  = "casal2_test.dll";
  *
  */
 int RunEstimationWithDll(int argc, char * argv[], niwa::utilities::RunParameters& options, const string& dll_name) {
-  auto library = LoadLibrary(dll_name.c_str());
+  auto library = dlopen(dll_name.c_str(), RTLD_LAZY);
   if (library == nullptr) {
     cout << "Error: Failed to load CASAL2 Library: " << dll_name << endl;
     return -1;
   }
 
-  auto proc = (RUNPROC)GetProcAddress(library, "Run");
+  auto proc = (RUNPROC)dlsym(library, "Run");
   return (proc)(argc, argv, options);
 }
 
@@ -51,27 +53,27 @@ int RunEstimationWithDll(int argc, char * argv[], niwa::utilities::RunParameters
  *
  */
 int RunUnitTests(int argc, char * argv[]) {
-  auto unit_test_library = LoadLibrary(test_dll.c_str());
+  auto unit_test_library = dlopen(test_dll.c_str(), RTLD_LAZY);
   if (unit_test_library == nullptr) {
     cout << "Error: Failed to load CASAL2 Unit Test DLL: " << test_dll << endl;
     return -1;
   }
 
-  auto unit_test_main_method =  (RUNTESTSPROC)GetProcAddress(unit_test_library, "RunUnitTests");
+  auto unit_test_main_method =  (RUNTESTSPROC)dlsym(unit_test_library, "RunUnitTests");
   if (unit_test_main_method == nullptr) {
     cout << "Error: Failed to get the main method address" << endl;
     return -1;
   }
 
   (unit_test_main_method)(argc, argv);
-  FreeLibrary(unit_test_library);
+  dlclose(unit_test_library);
   return 0;
 }
 
 /**
  *
  */
-int main(int argc, char * argv[]) {
+int run_for_os(int argc, char * argv[]) {
   for (int i = 0; i < argc; ++i) {
     if (string(argv[i]) == "--unittest") {
       cout << "Loading unit test DLL" << endl;
@@ -79,13 +81,13 @@ int main(int argc, char * argv[]) {
     }
   }
 
-  auto release_library = LoadLibrary(release_dll.c_str());
+  auto release_library = dlopen(release_dll.c_str(), RTLD_LAZY);
   if (release_library == nullptr) {
     cout << "Error: Failed to load CASAL2 Release Library: " << release_dll << endl;
     return -1;
   }
 
-  auto load_options = (LOADOPTIONSPROC)GetProcAddress(release_library, "LoadOptions");
+  auto load_options = (LOADOPTIONSPROC)dlsym(release_library, "LoadOptions");
   if (load_options == nullptr) {
     cout << "Failed to get the LoadOptions method address" << endl;
     return -1;
@@ -95,7 +97,7 @@ int main(int argc, char * argv[]) {
 
   return_code = (load_options)(argc, argv, options);
   if (return_code != 0) {
-    FreeLibrary(release_library);
+	  dlclose(release_library);
     return return_code;
   }
 
@@ -111,7 +113,7 @@ int main(int argc, char * argv[]) {
   case RunMode::kQuery:
   case RunMode::kTesting:
     {
-      auto main_method = (RUNPROC)GetProcAddress(release_library, "Run");
+      auto main_method = (RUNPROC)dlsym(release_library, "Run");
       if (main_method == nullptr) {
         cout << "Error: Failed to get the main method address" << endl;
         return_code = -1;
@@ -128,7 +130,7 @@ int main(int argc, char * argv[]) {
      * 3. Default to release if no matching DLL was found
      */
     {
-      auto preload_method = (PRELOADPROC)GetProcAddress(release_library, "PreParseConfigFiles");
+      auto preload_method = (PRELOADPROC)dlsym(release_library, "PreParseConfigFiles");
       if (preload_method == nullptr) {
         cout << "Error: Failed to get the preload method address" << endl;
         return_code = -1;
@@ -150,7 +152,7 @@ int main(int argc, char * argv[]) {
     break;
   }
 
-  FreeLibrary(release_library);
+  dlclose(release_library);
 	return return_code;
 }
-#endif /* __MINGW32__ */
+#endif
