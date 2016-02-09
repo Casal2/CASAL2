cc0143cbe7df733621692f0b8193a43855ee0dd9
Craig44 <Craig.Marsh@niwa.co.nz>
2016-02-10 08:35:33 +1300

    DEL: lower case functionality from Model/object.cpp

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 5976e71fe9b3d4294943fb204f8fd46d521930bd
-#define SOURCE_CONTROL_DATE "2016-02-04"
+#define SOURCE_CONTROL_REVISION 4c3940a25fbf483baaf7583fd6d57b863e70f500
+#define SOURCE_CONTROL_DATE "2016-02-09"
 #define SOURCE_CONTROL_YEAR "2016"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "23:46:35"
-#define SOURCE_CONTROL_VERSION "2016-02-04 23:46:35 UTC (rev. 5976e71)"
+#define SOURCE_CONTROL_TIME "08:29:25"
+#define SOURCE_CONTROL_VERSION "2016-02-09 08:29:25 UTC (rev. 4c3940a)"
 #endif
