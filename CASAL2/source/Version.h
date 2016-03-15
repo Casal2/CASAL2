7a4cf1c950dced1ea859f0e637caa8506aaa81d7
Craig44 <Craig.Marsh@niwa.co.nz>
2016-03-15 17:18:55 +1300

    CHG: Installer

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 6ba9250457250dd9541e5926004c2413be77eafd
+#define SOURCE_CONTROL_REVISION 8aff61f9328e74f858210449b1edf10055b8624a
 #define SOURCE_CONTROL_DATE "2016-03-14"
 #define SOURCE_CONTROL_YEAR "2016"
 #define SOURCE_CONTROL_MONTH "March"
-#define SOURCE_CONTROL_TIME "19:08:00"
-#define SOURCE_CONTROL_VERSION "2016-03-14 19:08:00 UTC (rev. 6ba9250)"
+#define SOURCE_CONTROL_TIME "19:51:10"
+#define SOURCE_CONTROL_VERSION "2016-03-14 19:51:10 UTC (rev. 8aff61f)"
 #endif
