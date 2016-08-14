5c65d3c84fcf475bb1ff9bbd81018c35062e6da1
Craig44 <Craig.Marsh@niwa.co.nz>
2016-08-15 08:36:25 +1200

    ADD: extra logging out for cppad builder

--- ThirdParty/cppad/linux.py
+++ ThirdParty/cppad/linux.py
@@ -37,10 +37,13 @@ class Builder:
     if os.path.exists(ipOptFileName + '.tgz'):
       os.system('gzip -dk ' + ipOptFileName + '.tgz 1> casal2_unzip.log 2>&1')
       os.system('tar xvf ' + ipOptFileName + '.tar 1> casal2_untar.log 2>&1')
+    print '-- cppadFileName' + cppadFileName  
     if os.path.exists(cppadFileName + '.zip'):
       os.system('unzip ' + cppadFileName + '.zip 1> casal2_unzip.log 2>&1')
     
     # Build
+    print '-- ipOptFileName' + ipOptFileName
+
     os.chdir(ipOptFileName + '/ThirdParty/ASL')      
     print '-- Building IpOPT Library'
     if os.system('./get.ASL 1> casal2_get.log 2>&1') != EX_OK:
