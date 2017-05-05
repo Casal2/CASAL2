10260acccf491fc93adab35e399352642c5599df
Craig44 <Craig.Marsh@niwa.co.nz>
2017-05-05 15:46:33 +1200

    FIX: doBuild script so that users can have spaces in path variable just add ""

--- BuildSystem/doBuild.bat
+++ BuildSystem/doBuild.bat
@@ -6,7 +6,7 @@ PATH=%PATH%;%CD%\buildtools\windows\unixutils\
 
 IF EXIST %CD%\buildtools\windows\python27 (echo Skipping Python Decompression) ELSE (echo Decompressing Python && unzip -qq buildtools\windows\python27.zip -d buildtools\windows\ && echo Python27 Decompressed)
 
-%CD%\buildtools\windows\python27\python build.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%CD%\buildtools\windows\python27\python" build.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 PATH=%oldPath%
 
