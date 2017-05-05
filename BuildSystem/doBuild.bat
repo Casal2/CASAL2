e245d27ba3a54cc61dd206712579bab185bfb971
Samik Datta <samik.datta@niwa.co.nz>
2017-05-05 16:30:58 +1200

    Add quotes to Python build line

--- BuildSystem/doBuild.bat
+++ BuildSystem/doBuild.bat
@@ -1,12 +1,11 @@
 @echo off
 
 set oldPath=%PATH%
-
 PATH=%PATH%;%CD%\buildtools\windows\unixutils\
 
-IF EXIST %CD%\buildtools\windows\python27 (echo Skipping Python Decompression) ELSE (echo Decompressing Python && unzip -qq buildtools\windows\python27.zip -d buildtools\windows\ && echo Python27 Decompressed)
+IF EXIST "%CD%\buildtools\windows\python27" (echo Skipping Python Decompression) ELSE (echo Decompressing Python && unzip -qq buildtools\windows\python27.zip -d buildtools\windows\ && echo Python27 Decompressed)
 
-"%CD%\buildtools\windows\python27\python" build.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%CD%\buildtools\windows\python27\python.exe" build.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 PATH=%oldPath%
 
