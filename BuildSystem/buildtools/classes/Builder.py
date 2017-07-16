621d3e9f43c0896100f6b0ca18fcbe9cc28c048b
Scott Rasmussen <scott@zaita.com>
2017-07-16 12:00:47 +1200

    FIX: Build scripts no longer thread build. This was breaking crappy cloud VM.

--- BuildSystem/buildtools/classes/Builder.py
+++ BuildSystem/buildtools/classes/Builder.py
@@ -1,4 +1,4 @@
-import os
+7import os
 import sys
 import subprocess
 import os.path
@@ -88,10 +88,10 @@ class MainCode:
     
     print "--> Build main code base"
     if Globals.operating_system_ == "windows":
-      if os.system("mingw32-make -j4") != EX_OK:
+      if os.system("mingw32-make") != EX_OK:
         return Globals.PrintError("Failed to build code base. Please see above for build error")
     else:
-      if os.system("make -j6") != EX_OK:
+      if os.system("make") != EX_OK:
         return Globals.PrintError("Failed to build code base. Please see above for build error")
 
     elapsed = time.time() - start
