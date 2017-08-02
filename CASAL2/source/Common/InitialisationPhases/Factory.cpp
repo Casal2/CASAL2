4c797a7fb3cec2c809ddaac596d902371ec5d98e
Scott Rasmussen <scott@zaita.com>
2017-08-02 18:23:26 +1200

    FIX: Merge didn't work

--- CASAL2/source/Common/InitialisationPhases/Factory.cpp
+++ CASAL2/source/Common/InitialisationPhases/Factory.cpp
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-/**
- * @file Factory.cpp
- * @author  Scott Rasmussen (scott.rasmussen@zaita.com)
- * @date 2/09/2014
- * @section LICENSE
- *
- * Copyright NIWA Science ©2014 - www.niwa.co.nz
- *
- */
-
-// headers
-#include "Factory.h"
-
-#include "Common/Model/Model.h"
-#include "Common/Model/Managers.h"
-#include "Age/InitialisationPhases/Factory.h"
-
-// Namespaces
-namespace niwa {
-namespace initialisationphases {
-
-/**
- * Create the instance of our object as defined by the two parameters
- * object_type and sub_type.
- *
- * @param object_type The type of object to create (e.g age_size, process)
- * @param sub_type The child type of the object to create (e.g ageing, schnute)
- * @return shared_ptr to the object we've created
- */
-InitialisationPhase* Factory::Create(Model* model, const string& object_type, const string& sub_type) {
-  InitialisationPhase* result = nullptr;
-
-  if (model->partition_structure() == PartitionStructure::kAge) {
-    result = age::initialisationphases::Factory::Create(model, object_type, sub_type);
-  }
-
-  return result;
-}
-
-}
-}
-
-
-=======
 /**
  * @file Factory.cpp
  * @author  Scott Rasmussen (scott.rasmussen@zaita.com)
@@ -93,4 +48,3 @@ InitialisationPhase* Factory::Create(Model* model, const string& object_type, co
 
 } /* namespace processes */
 } /* namespace niwa */
->>>>>>> 40376b86860a8959e98b6590d5321633b982d3c4
