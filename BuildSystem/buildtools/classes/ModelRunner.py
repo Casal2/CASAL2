64dad197aee839f260efd7df07c7ebd856fcb619
Craig Marsh <Craig.Marsh@niwa.co.nz>
2017-08-03 15:08:56 +1200

    ADD: more ModelRunner

--- BuildSystem/buildtools/classes/ModelRunner.py
+++ BuildSystem/buildtools/classes/ModelRunner.py
@@ -30,11 +30,15 @@ class ModelRunner:
     success_count = 0
     fail_count = 0
     estimation_dir_list = {"Simple", "TwoSex"}
+    dash_i_dir_list = {"Complex_input","TwoSex_input"}
     dir_list = os.listdir("../TestModels/")
     cwd = os.path.normpath(os.getcwd())  
+    # test -r functionality with full/different models
     for folder in dir_list:
       if folder == "TwoSex":
       	continue
+      if folder in dash_i_dir_list:
+        continue
       if folder.startswith("."):
         continue
       if folder.startswith("DO NOT"):
@@ -52,8 +56,20 @@ class ModelRunner:
         elapsed = time.time() - start
         print '[OK] - ' + folder + ' in ' + str(round(elapsed, 2)) + ' seconds'
         success_count += 1
-      os.chdir(cwd)
-      
+      os.chdir(cwd)   
+    # test -i functionality 
+    for folder in dash_i_dir_list:
+      os.chdir("../TestModels/" + folder)
+      if os.system("casal2 -r -i pars.out > run.log 2>&1") != EX_OK:
+        elapsed = time.time() - start
+        print '[FAILED] - ' + folder + ' -i run in ' + str(round(elapsed, 2)) + ' seconds'
+        fail_count += 1
+      else:
+        elapsed = time.time() - start
+        print '[OK] - ' + folder + ' -i run in ' + str(round(elapsed, 2)) + ' seconds'
+        success_count += 1
+      os.chdir(cwd) 
+    # test -e functionality
     for folder in estimation_dir_list:
       os.chdir("../TestModels/" + folder)
       if os.system("casal2 -e -g 20 > estimate.log 2>&1") != EX_OK:
@@ -65,7 +81,7 @@ class ModelRunner:
         print '[OK] - ' + folder + ' betadiff estimation in ' + str(round(elapsed, 2)) + ' seconds'
         success_count += 1
       os.chdir(cwd) 
-
+      
     print ''
     print 'Total Models: ' + str(success_count + fail_count)
     print 'Failed Models: ' + str(fail_count)
