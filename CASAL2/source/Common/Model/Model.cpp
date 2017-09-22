3b4797857d0b3ddf658f6a5ab6550ee3887af4c3
Craig Marsh <Craig.Marsh@niwa.co.nz>
2017-09-22 17:05:03 +1200

    UPD: Reports ADD: check estimates if they are close to bounds flag a warning during estimation

--- CASAL2/source/Common/Model/Model.cpp
+++ CASAL2/source/Common/Model/Model.cpp
@@ -524,6 +524,8 @@ void Model::RunEstimation() {
     }
 
     minimiser->BuildCovarianceMatrix();
+    // Check bounds
+    managers_->estimate()->CheckValuesRelativeToBounds();
 
     run_mode_ = RunMode::kBasic;
     FullIteration();
