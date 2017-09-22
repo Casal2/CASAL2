0664034375eceac283d9a093c1a5929e638f48d6
Craig Marsh <Craig.Marsh@niwa.co.nz>
2017-09-22 16:45:37 +1200

    UPD: R-Library so that extract functions code be used for alternative Encoding formats e.g. "UTF-16LE"

--- R-libraries/casal2/R/extract.parameters.R
+++ R-libraries/casal2/R/extract.parameters.R
@@ -5,12 +5,13 @@
 #' @author Craig Marsh
 #' @param file the name of the input file containing model output to extract
 #' @param path Optionally, the path to the file
+#' @param fileEncoding Optional, allows the R-library to read in files that have been encoded in alternative UTF formats, see the manual for the error message that would indicate when to use this switch.
 #' @return Data <"data.frame"> of parameters that are from a -i format.
 #' @export
 #'
-"extract.parameters" = function (file, path = "") {
+"extract.parameters" = function (file, path = "", fileEncoding = "") {
   filename = make.filename(path = path, file = file);
-  file = convert.to.lines(filename);
+  file = convert.to.lines(filename, fileEncoding = fileEncoding);
   ## remove tabs from header
   file[1] = gsub(pattern = "\t", replace = " ",x = file[1], fixed = TRUE)
   Data = make.data.frame(file);
