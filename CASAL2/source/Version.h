28d1d487238c5a0d3f16b76ced82c379183ccc8b
Craig Marsh <Craig.Marsh@niwa.co.nz>
2017-09-23 17:54:01 +1200

    RM: bounds check wasn't compatible with unit test

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 1272fff2e01aacb8471e8fc6faefb60f3b0ded5c
-#define SOURCE_CONTROL_DATE "2017-09-04"
+#define SOURCE_CONTROL_REVISION 8720d3f57349b89718d4a9d299d42f81e44d5adc
+#define SOURCE_CONTROL_DATE "2017-09-22"
 #define SOURCE_CONTROL_YEAR "2017"
 #define SOURCE_CONTROL_MONTH "September"
-#define SOURCE_CONTROL_TIME "21:45:11"
-#define SOURCE_CONTROL_VERSION "2017-09-04 21:45:11 UTC (rev. 1272fff2)"
+#define SOURCE_CONTROL_TIME "02:03:31"
+#define SOURCE_CONTROL_VERSION "2017-09-22 02:03:31 UTC (rev. 8720d3f5)"
 #endif
