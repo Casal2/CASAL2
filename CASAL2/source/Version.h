ab98a959a784f7822824756c0dde2deb4fd9f63d
Craig Marsh <Craig.Marsh@niwa.co.nz>
2017-12-06 18:32:49 +1300

    UPD: manual make the math notation consistent j = category, a = age, l = length, also clarified tagging process and tagRecapture Observation ADD: some math functions to Math.h that are taken from CASAL

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION b984d549b6cdc716934e3795612c048f23b1ae4e
-#define SOURCE_CONTROL_DATE "2017-11-29"
+#define SOURCE_CONTROL_REVISION 2d8d63f0fdde3c6a9d12b6293e70684493e1fc7b
+#define SOURCE_CONTROL_DATE "2017-12-01"
 #define SOURCE_CONTROL_YEAR "2017"
-#define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "06:59:14"
-#define SOURCE_CONTROL_VERSION "2017-11-29 06:59:14 UTC (rev. b984d549)"
+#define SOURCE_CONTROL_MONTH "December"
+#define SOURCE_CONTROL_TIME "07:20:18"
+#define SOURCE_CONTROL_VERSION "2017-12-01 07:20:18 UTC (rev. 2d8d63f0)"
 #endif
