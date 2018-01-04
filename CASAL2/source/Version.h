a25a222636a416dc4ef1db290b7a5ee24b46e3d3
Craig Marsh <Craig.Marsh@niwa.co.nz>
2018-01-04 19:37:05 +1300

    UPD: git commit guide ADD: unittest for age_weight class UPD: user manual for age weight info and examples

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 1942748d91bf9f176ba1f5482135f16779ec2f02
+#define SOURCE_CONTROL_REVISION 2bcd8fa3e98752b3877bfe2ecfc66d72ee64e12d
 #define SOURCE_CONTROL_DATE "2018-01-04"
 #define SOURCE_CONTROL_YEAR "2018"
 #define SOURCE_CONTROL_MONTH "January"
-#define SOURCE_CONTROL_TIME "05:23:48"
-#define SOURCE_CONTROL_VERSION "2018-01-04 05:23:48 UTC (rev. 1942748d)"
+#define SOURCE_CONTROL_TIME "05:50:18"
+#define SOURCE_CONTROL_VERSION "2018-01-04 05:50:18 UTC (rev. 2bcd8fa3)"
 #endif
