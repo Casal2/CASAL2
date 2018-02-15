59448a4b8c7830045c14526a39822bbb62df184e
Samik Datta <samik.datta@niwa.co.nz>
2018-02-15 16:53:12 +1300

    UPD: Accessing model fine now. TODO: Get transition matrix building correctly.

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION a8561360f1effd368b18dfbb6b390e95b89bfcac
-#define SOURCE_CONTROL_DATE "2018-02-07"
+#define SOURCE_CONTROL_REVISION bee5ad050cbdef7780654f37325e650c4b93c639
+#define SOURCE_CONTROL_DATE "2018-02-14"
 #define SOURCE_CONTROL_YEAR "2018"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "22:52:17"
-#define SOURCE_CONTROL_VERSION "2018-02-07 22:52:17 UTC (rev. a8561360)"
+#define SOURCE_CONTROL_TIME "03:28:30"
+#define SOURCE_CONTROL_VERSION "2018-02-14 03:28:30 UTC (rev. bee5ad05)"
 #endif
