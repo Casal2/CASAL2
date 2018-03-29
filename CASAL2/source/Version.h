6c419cd2950a6e788b64d1a4f857e9dd07f1ba5d
Samik Datta <samik.datta@niwa.co.nz>
2018-03-29 14:43:30 +1300

    UPD: Version.h

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 14cbc4437056a40842f393ce16da11ee05f0679c
-#define SOURCE_CONTROL_DATE "2018-02-16"
+#define SOURCE_CONTROL_REVISION d94dd4115bd968a8c025385107408b3ec964e9c5
+#define SOURCE_CONTROL_DATE "2018-02-23"
 #define SOURCE_CONTROL_YEAR "2018"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "01:37:19"
-#define SOURCE_CONTROL_VERSION "2018-02-16 01:37:19 UTC (rev. 14cbc443)"
+#define SOURCE_CONTROL_TIME "00:31:05"
+#define SOURCE_CONTROL_VERSION "2018-02-23 00:31:05 UTC (rev. d94dd411)"
 #endif
