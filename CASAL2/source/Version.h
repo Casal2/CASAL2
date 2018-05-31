c24581d9c07f79370e3c1f4bcb00ee3afeb003a4
Samik Datta <samik.datta@niwa.co.nz>
2018-05-31 12:00:45 +1200

    CHG: spelling errors in dictionary corrected.

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 9e3f478c78c0a9d4ca96f5c8992f851bb80ab7c5
-#define SOURCE_CONTROL_DATE "2018-04-08"
+#define SOURCE_CONTROL_REVISION 829ab24df98514ed3438556ba05f8f9dacb76171
+#define SOURCE_CONTROL_DATE "2018-05-30"
 #define SOURCE_CONTROL_YEAR "2018"
-#define SOURCE_CONTROL_MONTH "April"
-#define SOURCE_CONTROL_TIME "08:42:29"
-#define SOURCE_CONTROL_VERSION "2018-04-08 08:42:29 UTC (rev. 9e3f478c)"
+#define SOURCE_CONTROL_MONTH "May"
+#define SOURCE_CONTROL_TIME "05:29:47"
+#define SOURCE_CONTROL_VERSION "2018-05-30 05:29:47 UTC (rev. 829ab24d)"
 #endif
