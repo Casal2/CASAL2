f6e493f1490416eee0b34b1838d9a94d4ef1c8be
Alistair Dunn <alistairdunn1@gmail.com>
2018-08-22 20:59:34 +1200

    CHG: Grammar and spelling changes to the manual

--- build.bat
+++ build.bat
@@ -2,6 +2,9 @@ cd BuildSystem
 call doBuild.bat thirdparty
 call doBuild.bat debug
 call doBuild.bat release
-call doBuild.bat unittests
+call doBuild.bat test
 call doBuild.bat documentation
+call doBuild.bat archive
+call doBuild.bat rlibrary
+call doBuild.bat installer
 cd ..
