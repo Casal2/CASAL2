b38b712caf749cb9006bdd664386cfe98fdcbc7f
Craig44 <craig.marsh@bigbluedata.co.nz>
2018-08-27 10:00:31 +1200

    FIX: Report Factory call and include Catchability not Catchabilities

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 475e6a29476954a1f5a8b56172b09cc096e81158
-#define SOURCE_CONTROL_DATE "2018-08-10"
+#define SOURCE_CONTROL_REVISION 5984cfbac96293d13541b61fabfb24737cbfe0d6
+#define SOURCE_CONTROL_DATE "2018-08-24"
 #define SOURCE_CONTROL_YEAR "2018"
 #define SOURCE_CONTROL_MONTH "August"
-#define SOURCE_CONTROL_TIME "05:52:35"
-#define SOURCE_CONTROL_VERSION "2018-08-10 05:52:35 UTC (rev. 475e6a29)"
+#define SOURCE_CONTROL_TIME "02:51:48"
+#define SOURCE_CONTROL_VERSION "2018-08-24 02:51:48 UTC (rev. 5984cfba)"
 #endif
