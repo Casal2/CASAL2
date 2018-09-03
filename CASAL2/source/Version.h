729f5882c91e9af5ca6bcd9f740b45619ded3f07
Craig44 <craig.marsh@bigbluedata.co.nz>
2018-09-03 13:05:01 +1200

    CHG: Cross age matrix make it compatible with R library extract function. increase width to accommodate large partitions. ADD: the new report to the 2stock assessment

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 5984cfbac96293d13541b61fabfb24737cbfe0d6
-#define SOURCE_CONTROL_DATE "2018-08-24"
+#define SOURCE_CONTROL_REVISION 03d096b4fc601444a255a6d524ab340613676293
+#define SOURCE_CONTROL_DATE "2018-09-03"
 #define SOURCE_CONTROL_YEAR "2018"
-#define SOURCE_CONTROL_MONTH "August"
-#define SOURCE_CONTROL_TIME "02:51:48"
-#define SOURCE_CONTROL_VERSION "2018-08-24 02:51:48 UTC (rev. 5984cfba)"
+#define SOURCE_CONTROL_MONTH "September"
+#define SOURCE_CONTROL_TIME "00:27:50"
+#define SOURCE_CONTROL_VERSION "2018-09-03 00:27:50 UTC (rev. 03d096b4)"
 #endif
