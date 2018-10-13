e20a059e034fc00803f32aa26b0658d52513dea6
Samik Datta <samik.datta@niwa.co.nz>
2018-10-14 12:39:07 +1300

    UPD: Archiver.py altered so user manual name is changed from "UserManual.pdf" to "Casal2 User Manual.pdf". Also, some minor spelling corrections.

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 9c4d38bd3f78e437cd88f683c6e4be2a53fa3814
-#define SOURCE_CONTROL_DATE "2018-09-30"
+#define SOURCE_CONTROL_REVISION 125291f35e9abdad56dda2de2562b969c026eac1
+#define SOURCE_CONTROL_DATE "2018-10-10"
 #define SOURCE_CONTROL_YEAR "2018"
-#define SOURCE_CONTROL_MONTH "September"
-#define SOURCE_CONTROL_TIME "19:54:43"
-#define SOURCE_CONTROL_VERSION "2018-09-30 19:54:43 UTC (rev. 9c4d38bd)"
+#define SOURCE_CONTROL_MONTH "October"
+#define SOURCE_CONTROL_TIME "04:24:09"
+#define SOURCE_CONTROL_VERSION "2018-10-10 04:24:09 UTC (rev. 125291f3)"
 #endif
