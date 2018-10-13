06dbe1985165f6f50fb9af24a652e694cb3383ee
Samik Datta <samik.datta@niwa.co.nz>
2018-10-14 12:41:58 +1300

    UPD: Change 'skip_building' back to 'false'.

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 125291f35e9abdad56dda2de2562b969c026eac1
-#define SOURCE_CONTROL_DATE "2018-10-10"
+#define SOURCE_CONTROL_REVISION e20a059e034fc00803f32aa26b0658d52513dea6
+#define SOURCE_CONTROL_DATE "2018-10-13"
 #define SOURCE_CONTROL_YEAR "2018"
 #define SOURCE_CONTROL_MONTH "October"
-#define SOURCE_CONTROL_TIME "04:24:09"
-#define SOURCE_CONTROL_VERSION "2018-10-10 04:24:09 UTC (rev. 125291f3)"
+#define SOURCE_CONTROL_TIME "23:39:07"
+#define SOURCE_CONTROL_VERSION "2018-10-13 23:39:07 UTC (rev. e20a059e)"
 #endif
