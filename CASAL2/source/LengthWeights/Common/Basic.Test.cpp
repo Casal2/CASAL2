5d908d90965a65176bed3125c6bda506cc6f5bad
Marco Kienzle <marco.kienzle@gmail.com>
2018-10-25 14:06:11 +1300

    CHG: changing the expononent parameters in Basic3 test from 2 to 3

--- CASAL2/source/LengthWeights/Common/Basic.Test.cpp
+++ CASAL2/source/LengthWeights/Common/Basic.Test.cpp
@@ -1,7 +1,8 @@
 /**
  * @file Basic.Test.cpp
  * @author  Scott Rasmussen (scott.rasmussen@zaita.com)
- * @date 29/07/2013
+ * CREATED @date 29/07/2013
+ * MODIFIED 25/10/2018
  * @section LICENSE
  *
  * Copyright NIWA Science ©2013 - www.niwa.co.nz
@@ -82,17 +83,17 @@ TEST(LengthWeights, Basic3) {
   basic.parameters().Add(PARAM_LABEL, "unit_test_basic", __FILE__, __LINE__);
   basic.parameters().Add(PARAM_TYPE, "any value is fine", __FILE__, __LINE__);
   basic.parameters().Add(PARAM_A, "2", __FILE__, __LINE__);
-  basic.parameters().Add(PARAM_B, "2", __FILE__, __LINE__);
+  basic.parameters().Add(PARAM_B, "3", __FILE__, __LINE__);
   basic.parameters().Add(PARAM_UNITS, "tonnes", __FILE__, __LINE__);
 
   basic.Validate();
   basic.Build();
 
   EXPECT_DOUBLE_EQ(2, basic.mean_weight(1.0, AgeLength::Distribution::kNone, 1.0));
-  EXPECT_DOUBLE_EQ(8, basic.mean_weight(2.0, AgeLength::Distribution::kNone, 1.0));
-  EXPECT_DOUBLE_EQ(18, basic.mean_weight(3.0, AgeLength::Distribution::kNone, 1.0));
-  EXPECT_DOUBLE_EQ(32, basic.mean_weight(4.0, AgeLength::Distribution::kNone, 1.0));
-  EXPECT_DOUBLE_EQ(50, basic.mean_weight(5.0, AgeLength::Distribution::kNone, 1.0));
+  EXPECT_DOUBLE_EQ(16, basic.mean_weight(2.0, AgeLength::Distribution::kNone, 1.0));
+  EXPECT_DOUBLE_EQ(54, basic.mean_weight(3.0, AgeLength::Distribution::kNone, 1.0));
+  EXPECT_DOUBLE_EQ(128, basic.mean_weight(4.0, AgeLength::Distribution::kNone, 1.0));
+  EXPECT_DOUBLE_EQ(250, basic.mean_weight(5.0, AgeLength::Distribution::kNone, 1.0));
 }
 
 }
