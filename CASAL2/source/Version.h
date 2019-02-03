aa68fb565f4a45aa8fc0abf10f3a4bdddc905047
Samik Datta <samik.datta@niwa.co.nz>
2019-02-04 12:51:34 +1300

    Added predation electivity subsection to Mortality section of manual.

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 7a8d62aab69638a8fb18f629c1f7b02a35e15b85
-#define SOURCE_CONTROL_DATE "2019-01-28"
+#define SOURCE_CONTROL_REVISION 7c95b21a7f9cce9150b152ed349360d3aaea09d0
+#define SOURCE_CONTROL_DATE "2019-02-03"
 #define SOURCE_CONTROL_YEAR "2019"
-#define SOURCE_CONTROL_MONTH "January"
-#define SOURCE_CONTROL_TIME "00:51:08"
-#define SOURCE_CONTROL_VERSION "2019-01-28 00:51:08 UTC (rev. 7a8d62aa)"
+#define SOURCE_CONTROL_MONTH "February"
+#define SOURCE_CONTROL_TIME "21:09:48"
+#define SOURCE_CONTROL_VERSION "2019-02-03 21:09:48 UTC (rev. 7c95b21a)"
 #endif
