c03d4d53986517fc10c40cbbf1c989a6804c88ab
Scott Rasmussen <scott@zaita.com>
2019-02-04 18:14:25 +1300

    CHG: Update Google Test to 1.8.1 to fix MOCK_X errors

--- ThirdParty/googletest_googlemock/windows.py
+++ ThirdParty/googletest_googlemock/windows.py
@@ -5,15 +5,22 @@ from distutils import dir_util
 import Globals
 
 class Builder:
-  version_ = '1.6.0'
+  version_ = '1.8.1'
 
   def is_lean(self):
     return True
   
   def start(self):
     # Variables
-    gmockFileName = 'gmock-1.6.0'
-    library = 'gmock_main.a'
+    gmockFileName = 'googletest-release-1.8.1'
+    libraries = [ 
+      'gmock_main.a',
+      'libgmock_main.a',
+      'libgtest.a',
+      'libgmocka',
+      '02libgmock.a',
+      '01libgtest.a'
+      ]
     
     # Clean our any existing files if they already exist
     print '-- Cleaning files'
@@ -23,9 +30,10 @@ class Builder:
       shutil.rmtree(Globals.target_include_path_ + 'gmock')
     if os.path.exists(Globals.target_include_path_ + 'gtest'):
       shutil.rmtree(Globals.target_include_path_ + 'gtest')
-    os.system('rm -rf ' + Globals.target_debug_lib_path_ + library)
-    os.system('rm -rf ' + Globals.target_release_lib_path_ + library)
-        
+    for library in libraries:
+      os.system('rm -rf ' + Globals.target_debug_lib_path_ + library + '*')
+      os.system('rm -rf ' + Globals.target_release_lib_path_ + library + '*')  
+    
     # Decompress our archive
     print '-- Decompressing - check casal2_unzip.log'
     if os.path.exists(gmockFileName + '.zip'):
@@ -33,14 +41,17 @@ class Builder:
    
     # Build 
     print '-- Building - check ' + gmockFileName +'/make/casal2_build.log'
-    os.chdir(gmockFileName + '/make')
+    os.chdir(gmockFileName + '/googlemock')
+    os.system('cmake -G "MinGW Makefiles"')
     os.system("mingw32-make 1> casal2_build.log 2>&1")
         
     # Move our headers and libraries
     print '-- Moving headers and libraries'
-    shutil.copy('gmock_main.a', Globals.target_debug_lib_path_)
-    shutil.copy('gmock_main.a', Globals.target_release_lib_path_)
-    dir_util.copy_tree('../include/', Globals.target_include_path_)
-    dir_util.copy_tree('../gtest/include/', Globals.target_include_path_)
+    shutil.copy('libgmock.a', Globals.target_debug_lib_path_ + '/02libgmock.a')
+    shutil.copy('libgmock.a', Globals.target_release_lib_path_ + '/02libgmock.a')
+    shutil.copy('gtest/libgtest.a', Globals.target_debug_lib_path_ + '/01libgtest.a')
+    shutil.copy('gtest/libgtest.a', Globals.target_release_lib_path_ + '/01libgtest.a')
+    dir_util.copy_tree('include/', Globals.target_include_path_)
+    dir_util.copy_tree('../googletest/include/', Globals.target_include_path_)
     
     return True
