1df10a492a989180d36436f67edb5d456b4a5e26
Craig44 <craig.marsh@bigbluedata.co.nz>
2019-07-26 11:50:05 +1200

    UPD: Model to exclude the use of length_plus_group in age based Model UPD: TagRecaptureByLength

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 5a19611ad4810367be15ff4f40005207961b1483
-#define SOURCE_CONTROL_DATE "2019-07-24"
+#define SOURCE_CONTROL_REVISION 0bf3bad990afc4a354dfe47ef5e1070654b88905
+#define SOURCE_CONTROL_DATE "2019-07-25"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "00:14:44"
-#define SOURCE_CONTROL_VERSION "2019-07-24 00:14:44 UTC (rev. 5a19611a)"
+#define SOURCE_CONTROL_TIME "08:58:55"
+#define SOURCE_CONTROL_VERSION "2019-07-25 08:58:55 UTC (rev. 0bf3bad9)"
 #endif
