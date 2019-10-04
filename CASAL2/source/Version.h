cdb48e17bbfa521350d1e7b5f05dea41364d9324
Teresa Amar <zta@uw.edu>
2019-10-04 13:36:22 +1300

    UPD: rebuilt documentation and R package

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION a6908249e3dd2cd0601cd1dbd66e74f005a2e8c6
+#define SOURCE_CONTROL_REVISION 5f7393ad4721ee9c7a7c544f9b3205f6a8684bad
 #define SOURCE_CONTROL_DATE "2019-10-03"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "October"
-#define SOURCE_CONTROL_TIME "00:11:47"
-#define SOURCE_CONTROL_VERSION "2019-10-03 00:11:47 UTC (rev. a690824)"
+#define SOURCE_CONTROL_TIME "21:55:27"
+#define SOURCE_CONTROL_VERSION "2019-10-03 21:55:27 UTC (rev. 5f7393a)"
 #endif
