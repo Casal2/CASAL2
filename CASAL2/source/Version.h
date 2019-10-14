d13b6352f84b2d39eb0ce0e898a313b7f4d6d663
Teresa Amar <zta@uw.edu>
2019-10-15 12:29:12 +1300

    UPD: update docker file to build CppAD library and executable

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 7f6b70dde8b89d6b783102e9ec98bd5b5d231843
+#define SOURCE_CONTROL_REVISION 5b9d73feab91cafefb051213fa0cc13810f6fe3d
 #define SOURCE_CONTROL_DATE "2019-10-14"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "October"
-#define SOURCE_CONTROL_TIME "00:06:05"
-#define SOURCE_CONTROL_VERSION "2019-10-14 00:06:05 UTC (rev. 7f6b70d)"
+#define SOURCE_CONTROL_TIME "20:56:46"
+#define SOURCE_CONTROL_VERSION "2019-10-14 20:56:46 UTC (rev. 5b9d73f)"
 #endif
