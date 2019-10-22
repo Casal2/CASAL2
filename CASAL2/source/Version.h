8ca170914b105fe60cbf317f4dccac862512c097
Teresa Amar <zta@uw.edu>
2019-10-22 14:09:01 +1300

    UPD: updated documentation for retained catch functionality

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION ed45ee5ca1f3cac4cbdceb3f7ac17cc7f0e63217
-#define SOURCE_CONTROL_DATE "2019-10-21"
+#define SOURCE_CONTROL_REVISION 7d0b6a35a72f8c851b44724ec598ad06737688e6
+#define SOURCE_CONTROL_DATE "2019-10-22"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "October"
-#define SOURCE_CONTROL_TIME "01:08:58"
-#define SOURCE_CONTROL_VERSION "2019-10-21 01:08:58 UTC (rev. ed45ee5)"
+#define SOURCE_CONTROL_TIME "00:24:30"
+#define SOURCE_CONTROL_VERSION "2019-10-22 00:24:30 UTC (rev. 7d0b6a3)"
 #endif
