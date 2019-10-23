7123a3620da3c852fdd9a3507505a110e0fe6d75
Craig44 <craig.marsh@bigbluedata.co.nz>
2019-10-23 23:46:10 +1300

    ADD: function to deal with data frames with row labels

---
+++ R-libraries/casal2/R/make.data_frame_with_row_labs.R
@@ -0,0 +1,29 @@
+#' Utility extract function
+#'
+#' @author C Marsh
+#' @keywords internal
+#'
+make.data_frame_with_row_labs <- function(lines) {
+  columns <- string.to.vector.of.words(lines[1])[-1] ## remove the first element it should be label of rows
+  data <- data.frame(matrix(nrow=length(lines)-1,ncol=length(columns)))
+  row_labs = vector();
+  for(i in 2:length(lines)) {
+    line = string.to.vector.of.words(lines[i])
+    row_labs = c(row_labs, line[1])
+    line = line[-1]
+    if (length(line) != length(columns)) {
+        stop(paste(lines[i],"is not the same length as",lines[1]))
+    }
+    
+    data[i-1,] <- line
+  }
+  colnames(data) <- columns
+  rownames(data) = row_labs;
+  for (i in 1:ncol(data)) {
+      if (is.all.numeric(data[,i])) {
+          data[,i]=as.numeric(data[,i])
+      }
+  }
+  data
+}
+
