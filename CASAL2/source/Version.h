440eac7e9eb997d1c7da219a38b14603ae284bbc
Teresa Amar <zta@uw.edu>
2019-10-29 14:09:25 +1300

    UPD: minor changes for consistent usage of length_plus vs. length_plus_group

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION f1256a60dd216387a5a50f6dc756affb27b60bf2
-#define SOURCE_CONTROL_DATE "2019-10-23"
+#define SOURCE_CONTROL_REVISION 80e6a26c4d6140fb00c31d67638301c302f4aa06
+#define SOURCE_CONTROL_DATE "2019-10-24"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "October"
-#define SOURCE_CONTROL_TIME "23:50:39"
-#define SOURCE_CONTROL_VERSION "2019-10-23 23:50:39 UTC (rev. f1256a6)"
+#define SOURCE_CONTROL_TIME "21:23:29"
+#define SOURCE_CONTROL_VERSION "2019-10-24 21:23:29 UTC (rev. 80e6a26)"
 #endif
