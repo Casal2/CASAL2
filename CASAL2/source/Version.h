1b1ed731537dc551674c911da3bf387273a97a92
Teresa Amar <zta@uw.edu>
2019-11-18 12:35:35 +1300

    UPD: more changes for AD Double to double for ADOL-C

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 0d4d7296dd1fdddeaeb7a1a24d7fa8872b59c93b
-#define SOURCE_CONTROL_DATE "2019-11-15"
+#define SOURCE_CONTROL_REVISION 592868c6f24bb3173fb1e4a11ef87609caa3dd9a
+#define SOURCE_CONTROL_DATE "2019-11-17"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "05:39:51"
-#define SOURCE_CONTROL_VERSION "2019-11-15 05:39:51 UTC (rev. 0d4d729)"
+#define SOURCE_CONTROL_TIME "23:02:07"
+#define SOURCE_CONTROL_VERSION "2019-11-17 23:02:07 UTC (rev. 592868c)"
 #endif
