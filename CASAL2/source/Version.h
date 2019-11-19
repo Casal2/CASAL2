acd4415cf17a0de21f7a6fc5c2818728015da043
Teresa Amar <zta@uw.edu>
2019-11-19 15:37:33 +1300

    UPD: test case estimation model output files

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 2066eff9122daef3dc89c6dd01bdd09d06e9977a
+#define SOURCE_CONTROL_REVISION cb93762761d85676d6fbfb2d20aa9ea5c5e6e209
 #define SOURCE_CONTROL_DATE "2019-11-19"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "02:17:08"
-#define SOURCE_CONTROL_VERSION "2019-11-19 02:17:08 UTC (rev. 2066eff)"
+#define SOURCE_CONTROL_TIME "02:20:53"
+#define SOURCE_CONTROL_VERSION "2019-11-19 02:20:53 UTC (rev. cb93762)"
 #endif
