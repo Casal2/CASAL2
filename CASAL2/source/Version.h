43b4c7dc87e4bf3c14b198f9e0e8b672a5550a37
Teresa Amar <zta@uw.edu>
2019-11-19 16:29:11 +1300

    UPD: update of file mpd.out for TestModels

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION cb93762761d85676d6fbfb2d20aa9ea5c5e6e209
+#define SOURCE_CONTROL_REVISION acd4415cf17a0de21f7a6fc5c2818728015da043
 #define SOURCE_CONTROL_DATE "2019-11-19"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "02:20:53"
-#define SOURCE_CONTROL_VERSION "2019-11-19 02:20:53 UTC (rev. cb93762)"
+#define SOURCE_CONTROL_TIME "02:37:33"
+#define SOURCE_CONTROL_VERSION "2019-11-19 02:37:33 UTC (rev. acd4415)"
 #endif
