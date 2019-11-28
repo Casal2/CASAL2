2ebd7c0a980c84ef1b9951d9a792081a417a3f65
Teresa Amar <zta@uw.edu>
2019-11-28 14:42:24 +1300

    UPD: adjust subdir names and run scripts for test cases

--- TestCases/primary/LIN56/run_models.bat
+++ TestCases/primary/LIN56/run_models.bat
@@ -9,15 +9,30 @@ casal -e -o params_est.out > run_estimation.txt 2>&1
 cd ..\CASAL_sens1
 casal -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ..\Casal2\casal_flags_on
+
+cd ..\Casal2\adolc_casal_flags_off
+..\..\..\..\..\BuildSystem\bin\windows\release_adolc\casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ..\adolc_casal_flags_on
+..\..\..\..\..\BuildSystem\bin\windows\release_adolc\casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ..\adolc_casal_flags_on_low_tol
+..\..\..\..\..\BuildSystem\bin\windows\release_adolc\casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+
+cd ..\betadiff_casal_flags_off
 ..\..\..\..\..\BuildSystem\bin\windows\release_betadiff\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ..\casal_flags_on_low_tol
+cd ..\betadiff_casal_flags_on
 ..\..\..\..\..\BuildSystem\bin\windows\release_betadiff\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ..\casal_flags_off
+cd ..\betadiff_casal_flags_on_low_tol
 ..\..\..\..\..\BuildSystem\bin\windows\release_betadiff\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ..\cppad
+
+cd ..\cppad_casal_flags_off
+..\..\..\..\..\BuildSystem\bin\windows\release_cppad\casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ..\cppad_casal_flags_on
 ..\..\..\..\..\BuildSystem\bin\windows\release_cppad\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
--- TestCases/primary/LIN56/run_models.sh
+++ TestCases/primary/LIN56/run_models.sh
@@ -9,18 +9,29 @@ cd ../CASAL_sens1
 
 
 
-cd ../Casal2/adolc
+cd ../Casal2/adolc_casal_flags_off
 ../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ../casal_flag_on
+cd ../adolc_casal_flags_on
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../adolc_casal_flags_on_low_tol
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+
+cd ../betadiff_casal_flags_off
 ../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ../casal_flag_on_low_tol
+cd ../betadiff_casal_flags_on
 ../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ../casal_flag_off
+cd ../betadiff_casal_flags_on_low_tol
 ../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ../cppad
+
+cd ../cppad_casal_flags_off
+../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../cppad_casal_flags_on
 ../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
