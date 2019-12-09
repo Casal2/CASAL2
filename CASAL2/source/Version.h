deb4f2972929b80b1d0e940d7f2e6d4274e0a29c
Teresa Amar <zta@uw.edu>
2019-12-09 14:39:37 +1300

    UPD: update documentation

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION f330f8faf6d0ec850b0f04b6bfad2d88908fb7f5
-#define SOURCE_CONTROL_DATE "2019-12-08"
+#define SOURCE_CONTROL_REVISION 3c71b0428d9eaad20fafbb94184468a4f2d7c21e
+#define SOURCE_CONTROL_DATE "2019-12-09"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "December"
-#define SOURCE_CONTROL_TIME "21:27:10"
-#define SOURCE_CONTROL_VERSION "2019-12-08 21:27:10 UTC (rev. f330f8f)"
+#define SOURCE_CONTROL_TIME "00:43:09"
+#define SOURCE_CONTROL_VERSION "2019-12-09 00:43:09 UTC (rev. 3c71b04)"
 #endif
