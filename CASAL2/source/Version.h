0d0c2815c4f5fa5fc74622f43bc13e921ad56a39
Teresa Amar <zta@uw.edu>
2019-12-10 11:34:53 +1300

    UPD: unmangle merged version of source/Version.h

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,28 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-<<<<<<< HEAD
-#define SOURCE_CONTROL_REVISION 3c71b0428d9eaad20fafbb94184468a4f2d7c21e
+#define SOURCE_CONTROL_REVISION 847d7232f4691143a884530b2a8ff28f4c11fea1
 #define SOURCE_CONTROL_DATE "2019-12-09"
-||||||| merged common ancestors
-#define SOURCE_CONTROL_REVISION 99d95d899430f2f6b3a003195c4a53ee4f6772d8
-#define SOURCE_CONTROL_DATE "2019-11-28"
-=======
-#define SOURCE_CONTROL_REVISION 7841476940f0649213dcf5883ea2ff4a8f9efe30
-#define SOURCE_CONTROL_DATE "2019-11-29"
->>>>>>> master
 #define SOURCE_CONTROL_YEAR "2019"
-<<<<<<< HEAD
 #define SOURCE_CONTROL_MONTH "December"
-#define SOURCE_CONTROL_TIME "00:43:09"
-#define SOURCE_CONTROL_VERSION "2019-12-09 00:43:09 UTC (rev. 3c71b04)"
-||||||| merged common ancestors
-#define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "04:21:37"
-#define SOURCE_CONTROL_VERSION "2019-11-28 04:21:37 UTC (rev. 99d95d8)"
-=======
-#define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "04:37:52"
-#define SOURCE_CONTROL_VERSION "2019-11-29 04:37:52 UTC (rev. 7841476)"
->>>>>>> master
+#define SOURCE_CONTROL_TIME "04:05:24"
+#define SOURCE_CONTROL_VERSION "2019-12-09 04:05:24 UTC (rev. 847d723)"
 #endif
