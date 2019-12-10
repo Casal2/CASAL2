e39febd4e9bbe31f18f37ab447867d99645dd5dc
Teresa Amar <zta@uw.edu>
2019-12-11 12:36:52 +1300

    UPD: first pass through User Manual Troubleshooting section

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 847d7232f4691143a884530b2a8ff28f4c11fea1
+#define SOURCE_CONTROL_REVISION 0d0c2815c4f5fa5fc74622f43bc13e921ad56a39
 #define SOURCE_CONTROL_DATE "2019-12-09"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "December"
-#define SOURCE_CONTROL_TIME "04:05:24"
-#define SOURCE_CONTROL_VERSION "2019-12-09 04:05:24 UTC (rev. 847d723)"
+#define SOURCE_CONTROL_TIME "22:34:53"
+#define SOURCE_CONTROL_VERSION "2019-12-09 22:34:53 UTC (rev. 0d0c281)"
 #endif
