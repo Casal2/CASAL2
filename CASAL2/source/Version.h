c31aa604b215367817b23d950493824264fe8f93
Teresa Amar <zta@uw.edu>
2019-12-18 13:05:22 +1300

    UPD: completion of first pass through User Manual Population section

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 0d0c2815c4f5fa5fc74622f43bc13e921ad56a39
-#define SOURCE_CONTROL_DATE "2019-12-09"
+#define SOURCE_CONTROL_REVISION b79a1d8c12a7eabe595f7b415d1aee59104a6668
+#define SOURCE_CONTROL_DATE "2019-12-17"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "December"
-#define SOURCE_CONTROL_TIME "22:34:53"
-#define SOURCE_CONTROL_VERSION "2019-12-09 22:34:53 UTC (rev. 0d0c281)"
+#define SOURCE_CONTROL_TIME "04:35:51"
+#define SOURCE_CONTROL_VERSION "2019-12-17 04:35:51 UTC (rev. b79a1d8)"
 #endif
