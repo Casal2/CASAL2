fbf51210c2c135dd525fd753d496cbd3d18fad3a
Teresa Amar <zta@uw.edu>
2020-01-10 15:56:50 +1300

    UPD: changed objective function comparison tolerance from 1e-5 to 1e-6

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 3b5fa984645f99f66c7a7b33855497e97ab9af58
-#define SOURCE_CONTROL_DATE "2020-01-05"
+#define SOURCE_CONTROL_REVISION b38f3e1182be54cc4293cabea8d9057df85251a2
+#define SOURCE_CONTROL_DATE "2020-01-07"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "January"
-#define SOURCE_CONTROL_TIME "22:58:18"
-#define SOURCE_CONTROL_VERSION "2020-01-05 22:58:18 UTC (rev. 3b5fa98)"
+#define SOURCE_CONTROL_TIME "02:03:32"
+#define SOURCE_CONTROL_VERSION "2020-01-07 02:03:32 UTC (rev. b38f3e1)"
 #endif
