6a2b47be0be0bbb912db94d7ae74111329aaf14c
Teresa Amar <zta@uw.edu>
2020-01-21 13:02:26 +1300

    UPD: delete existing params_est.out files for CASAL and Casal2 test case models

--- TestCases/primary/LIN56/run_models.bat
+++ TestCases/primary/LIN56/run_models.bat
@@ -3,35 +3,45 @@ rem this assumes that 'release adolc', 'release betadiff', and 'release cppad' h
 rem this script runs in directory TestCases\primary\LIN56
 
 cd CASAL
+del params_est.out
 ..\..\..\casal.exe -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ..\CASAL_sens1
+del params_est.out
 ..\..\..\casal.exe -e -o params_est.out > run_estimation.txt 2>&1
 
 
 cd ..\Casal2\adolc_casal_flags_off
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_adolc\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ..\adolc_casal_flags_on
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_adolc\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ..\adolc_casal_flags_on_low_tol
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_adolc\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 
 cd ..\betadiff_casal_flags_off
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_betadiff\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ..\betadiff_casal_flags_on
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_betadiff\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ..\betadiff_casal_flags_on_low_tol
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_betadiff\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 
 cd ..\cppad_casal_flags_off
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_cppad\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ..\cppad_casal_flags_on
+del params_est.out
 ..\..\..\..\..\BuildSystem\bin\windows\release_cppad\casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
