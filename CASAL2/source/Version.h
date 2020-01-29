42f44dbcefa1e1b64155388205297dc5e6dfd767
Teresa Amar <zta@uw.edu>
2020-01-30 12:50:41 +1300

    UPD: ensure that models in Example directory run (issue #34)

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 18193815f3d7f6d82bba3ae6d9221815c168ac74
+#define SOURCE_CONTROL_REVISION bba18d6fd1d90fbf3a7ac7b11a15aa100a9179e0
 #define SOURCE_CONTROL_DATE "2020-01-28"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "January"
-#define SOURCE_CONTROL_TIME "02:30:18"
-#define SOURCE_CONTROL_VERSION "2020-01-28 02:30:18 UTC (rev. 1819381)"
+#define SOURCE_CONTROL_TIME "21:03:02"
+#define SOURCE_CONTROL_VERSION "2020-01-28 21:03:02 UTC (rev. bba18d6)"
 #endif
