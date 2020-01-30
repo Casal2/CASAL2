f82703160cba39b685a734635ac6a7be403b1e9f
Teresa Amar <zta@uw.edu>
2020-01-30 17:49:24 +1300

    ADD: add R markdown Test Case LIN5/6 file

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 0ffeb1355e7d8a243ad2ff1803cb350751855839
+#define SOURCE_CONTROL_REVISION dbe7bcbd11d4e474e0bbce8750f0df083bad953d
 #define SOURCE_CONTROL_DATE "2020-01-30"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "January"
-#define SOURCE_CONTROL_TIME "00:11:53"
-#define SOURCE_CONTROL_VERSION "2020-01-30 00:11:53 UTC (rev. 0ffeb13)"
+#define SOURCE_CONTROL_TIME "02:22:22"
+#define SOURCE_CONTROL_VERSION "2020-01-30 02:22:22 UTC (rev. dbe7bcb)"
 #endif
