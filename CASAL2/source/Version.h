486e09755ba6cfe91f0f39d7aad6b9dcd4dc5234
Teresa Amar <zta@uw.edu>
2020-02-04 10:33:45 +1300

    UPD: Test Case R markdown HOK and LIN5/6  message cleanup

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION dbe7bcbd11d4e474e0bbce8750f0df083bad953d
-#define SOURCE_CONTROL_DATE "2020-01-30"
+#define SOURCE_CONTROL_REVISION 78c27747909e372469ea4a8c2aa4494441cc1399
+#define SOURCE_CONTROL_DATE "2020-02-03"
 #define SOURCE_CONTROL_YEAR "2020"
-#define SOURCE_CONTROL_MONTH "January"
-#define SOURCE_CONTROL_TIME "02:22:22"
-#define SOURCE_CONTROL_VERSION "2020-01-30 02:22:22 UTC (rev. dbe7bcb)"
+#define SOURCE_CONTROL_MONTH "February"
+#define SOURCE_CONTROL_TIME "00:04:54"
+#define SOURCE_CONTROL_VERSION "2020-02-03 00:04:54 UTC (rev. 78c2774)"
 #endif
