3dd55edad3961d6ed993a0dea87fa454b285b2c8
Teresa Amar <zta@uw.edu>
2020-02-03 18:05:49 +1300

    UPD: first pass of R library build in branch 02022020_rlibrary_doc

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 08a0edc54084002ae1437d0dc82922865b3f943f
-#define SOURCE_CONTROL_DATE "2020-02-02"
+#define SOURCE_CONTROL_REVISION cd250daa557f465b53bff8dc56676504057792e6
+#define SOURCE_CONTROL_DATE "2020-02-03"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "22:10:56"
-#define SOURCE_CONTROL_VERSION "2020-02-02 22:10:56 UTC (rev. 08a0edc)"
+#define SOURCE_CONTROL_TIME "04:09:01"
+#define SOURCE_CONTROL_VERSION "2020-02-03 04:09:01 UTC (rev. cd250da)"
 #endif
