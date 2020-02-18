9e62aa5b12fb859c944e87a84516f0efcae71a50
Teresa Amar <zta@uw.edu>
2020-02-19 12:33:54 +1300

    UPD: forgot to add header guard to ADOL-C operator redefinition

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION aaa52bdb49065ccfca8185a363b5543f459c1df8
-#define SOURCE_CONTROL_DATE "2020-02-17"
+#define SOURCE_CONTROL_REVISION cc2e94d1e0079dfa2c73f03312b75031a7bb1253
+#define SOURCE_CONTROL_DATE "2020-02-18"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "22:52:19"
-#define SOURCE_CONTROL_VERSION "2020-02-17 22:52:19 UTC (rev. aaa52bd)"
+#define SOURCE_CONTROL_TIME "04:43:56"
+#define SOURCE_CONTROL_VERSION "2020-02-18 04:43:56 UTC (rev. cc2e94d)"
 #endif
