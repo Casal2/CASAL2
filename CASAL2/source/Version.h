8215d661958dc963ee6fe88e9a7e52758b882ed7
Teresa Amar <zta@uw.edu>
2020-02-21 15:33:49 +1300

    UPD: reference CppAD variables not parameters for objective function

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 178cc2ddb0b29c403a879ff63d9b67a84ea23bfc
+#define SOURCE_CONTROL_REVISION 9ad7c37d7417dc294822c4406808b4fbab98448e
 #define SOURCE_CONTROL_DATE "2020-02-21"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "01:32:09"
-#define SOURCE_CONTROL_VERSION "2020-02-21 01:32:09 UTC (rev. 178cc2d)"
+#define SOURCE_CONTROL_TIME "01:56:07"
+#define SOURCE_CONTROL_VERSION "2020-02-21 01:56:07 UTC (rev. 9ad7c37)"
 #endif
