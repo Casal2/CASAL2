728f15429f932629c5a8bddd373e7116a8d4257b
Teresa Amar <zta@uw.edu>
2020-03-02 12:48:39 +1300

    UPD: make change to ADOL-C << operator in library code

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 04e913cafa10a7bb16a89504eee40e5af1fd1cee
-#define SOURCE_CONTROL_DATE "2020-02-27"
+#define SOURCE_CONTROL_REVISION 6b62329acd574980128f111b5a9da0be97dcd116
+#define SOURCE_CONTROL_DATE "2020-02-28"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "03:11:05"
-#define SOURCE_CONTROL_VERSION "2020-02-27 03:11:05 UTC (rev. 04e913c)"
+#define SOURCE_CONTROL_TIME "03:37:35"
+#define SOURCE_CONTROL_VERSION "2020-02-28 03:37:35 UTC (rev. 6b62329)"
 #endif
