8bc99dc3f317be0d400a3c560dea894426d0621c
Teresa Amar <zta@uw.edu>
2020-03-10 12:08:52 +1300

    UPD: add MCMC covariance matrix report to Test Case Casal2 models

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION e40c5a6a79370f6b955c588c76e7190d0604b788
-#define SOURCE_CONTROL_DATE "2020-03-02"
+#define SOURCE_CONTROL_REVISION f64e2a9a8bcbc99c59415b1284cbb976f100929d
+#define SOURCE_CONTROL_DATE "2020-03-04"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "March"
-#define SOURCE_CONTROL_TIME "23:07:21"
-#define SOURCE_CONTROL_VERSION "2020-03-02 23:07:21 UTC (rev. e40c5a6)"
+#define SOURCE_CONTROL_TIME "04:39:19"
+#define SOURCE_CONTROL_VERSION "2020-03-04 04:39:19 UTC (rev. f64e2a9)"
 #endif
