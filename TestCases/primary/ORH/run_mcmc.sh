ea3d0714b16817af7f9a5c3804e7ade447c6ef9d
Teresa Amar <zta@uw.edu>
2020-03-10 16:26:25 +1300

    UPD: update Test Case estimation output and MCMC characteristics

---
+++ TestCases/primary/ORH/run_mcmc.sh
@@ -0,0 +1,47 @@
+# this assumes that 'release adolc', 'release betadiff', and 'release cppad' have already been built successfully
+# and that the estimation runs have completed successfully (i.e., mpd.out exists for the Casal2 models)
+# start in directory TestCases/primary/[assessment]
+
+cd CASAL
+rm -f params_mcmc.out objectives.* samples.*
+../../../casal -m -o params_mcmc.out > run_mcmc.txt 2>&1 &
+
+cd ../CASAL_sens1
+rm -f params_mcmc.out objectives.* samples.*
+../../../casal -m -o params_mcmc.out > run_mcmc.txt 2>&1 &
+
+
+cd ../Casal2/betadiff_casal_flags_off
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+cd ../betadiff_casal_flags_on
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+cd ../betadiff_casal_flags_on_low_tol
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+
+cd ../cppad_casal_flags_off
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_cppad/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+cd ../cppad_casal_flags_on
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_cppad/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+
+cd ../adolc_casal_flags_off
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+cd ../adolc_casal_flags_on
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
+cd ../adolc_casal_flags_on_low_tol
+rm -f params_mcmc.out mcmc*.out
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -m --skip-estimation > run_mcmc.txt 2>&1 &
+
