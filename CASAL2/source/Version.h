454b74b6b8fb50ed5e3ab42832004c0d2555d983
Teresa Amar <zta@uw.edu>
2020-03-30 13:34:44 +1300

    UPD: update R library R Markdown file

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION feb0f27954d27064a2d1c156a63ceea14db11348
-#define SOURCE_CONTROL_DATE "2020-03-26"
+#define SOURCE_CONTROL_REVISION ca819f5efa33d00e75cc42f5e4068d7f735adf21
+#define SOURCE_CONTROL_DATE "2020-03-27"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "March"
-#define SOURCE_CONTROL_TIME "22:45:10"
-#define SOURCE_CONTROL_VERSION "2020-03-26 22:45:10 UTC (rev. feb0f27)"
+#define SOURCE_CONTROL_TIME "04:18:02"
+#define SOURCE_CONTROL_VERSION "2020-03-27 04:18:02 UTC (rev. ca819f5)"
 #endif
