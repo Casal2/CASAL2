9fd79c873dd499208a6d27530c79f7b9aa56252a
Teresa Amar <zta@uw.edu>
2020-04-08 15:55:44 +1200

    UPD: updated a few doxygen comments for Likelihoods/Common

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 443b9af79b974686c117c0cfeccab741def360d7
+#define SOURCE_CONTROL_REVISION 6d4db156acc88fa7dc5bdffdd73f9768855cfb5c
 #define SOURCE_CONTROL_DATE "2020-04-07"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "April"
-#define SOURCE_CONTROL_TIME "06:21:28"
-#define SOURCE_CONTROL_VERSION "2020-04-07 06:21:28 UTC (rev. 443b9af)"
+#define SOURCE_CONTROL_TIME "22:55:21"
+#define SOURCE_CONTROL_VERSION "2020-04-07 22:55:21 UTC (rev. 6d4db15)"
 #endif
