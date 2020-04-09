f39374914648fc44f3f102d13fdb172ad3099f92
Teresa Amar <zta@uw.edu>
2020-04-09 14:11:35 +1200

    UPD: increased max correlation in Test Case models so that MCMC runs

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 6d4db156acc88fa7dc5bdffdd73f9768855cfb5c
-#define SOURCE_CONTROL_DATE "2020-04-07"
+#define SOURCE_CONTROL_REVISION 9fd79c873dd499208a6d27530c79f7b9aa56252a
+#define SOURCE_CONTROL_DATE "2020-04-08"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "April"
-#define SOURCE_CONTROL_TIME "22:55:21"
-#define SOURCE_CONTROL_VERSION "2020-04-07 22:55:21 UTC (rev. 6d4db15)"
+#define SOURCE_CONTROL_TIME "03:55:44"
+#define SOURCE_CONTROL_VERSION "2020-04-08 03:55:44 UTC (rev. 9fd79c8)"
 #endif
