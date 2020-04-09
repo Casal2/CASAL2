94766dfd15d33594f8517dd7089b35479d9344b0
Teresa Amar <zta@uw.edu>
2020-04-09 14:54:28 +1200

    UPD: updated User Manual with length bin specification information

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 9fd79c873dd499208a6d27530c79f7b9aa56252a
-#define SOURCE_CONTROL_DATE "2020-04-08"
+#define SOURCE_CONTROL_REVISION 78254abfae813fd99ec72f1816295cc3f1556b9d
+#define SOURCE_CONTROL_DATE "2020-04-09"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "April"
-#define SOURCE_CONTROL_TIME "03:55:44"
-#define SOURCE_CONTROL_VERSION "2020-04-08 03:55:44 UTC (rev. 9fd79c8)"
+#define SOURCE_CONTROL_TIME "02:22:31"
+#define SOURCE_CONTROL_VERSION "2020-04-09 02:22:31 UTC (rev. 78254ab)"
 #endif
