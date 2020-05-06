039eef05e5279717a61c693b1d680bdadf5df2f8
Teresa Amar <zta@uw.edu>
2020-05-06 17:13:36 +1200

    UPD: update Test Model and Test Case LIN5/6 mpd.out

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 94cfd2035e7f5e10c2f489e06ae62e330937177a
+#define SOURCE_CONTROL_REVISION 8764e9e8d3b60c337729542910bc2005e773d82a
 #define SOURCE_CONTROL_DATE "2020-05-06"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "May"
-#define SOURCE_CONTROL_TIME "02:02:05"
-#define SOURCE_CONTROL_VERSION "2020-05-06 02:02:05 UTC (rev. 94cfd20)"
+#define SOURCE_CONTROL_TIME "04:49:42"
+#define SOURCE_CONTROL_VERSION "2020-05-06 04:49:42 UTC (rev. 8764e9e)"
 #endif
