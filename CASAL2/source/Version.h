e178886e4fe1aefa56cdd965867232a83416e3ee
Teresa Amar <zta@uw.edu>
2020-05-08 16:49:22 +1200

    ADD: add Test Case ORH report

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 4faf323f387a6a586ab3255e6173d7c4d2662b2d
+#define SOURCE_CONTROL_REVISION 3a28f80490b5f097e3a66db504f36cb39ac03500
 #define SOURCE_CONTROL_DATE "2020-05-07"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "May"
-#define SOURCE_CONTROL_TIME "04:16:35"
-#define SOURCE_CONTROL_VERSION "2020-05-07 04:16:35 UTC (rev. 4faf323)"
+#define SOURCE_CONTROL_TIME "04:55:59"
+#define SOURCE_CONTROL_VERSION "2020-05-07 04:55:59 UTC (rev. 3a28f80)"
 #endif
