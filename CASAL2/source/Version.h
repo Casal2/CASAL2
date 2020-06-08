0df821a6b29d33d9e2e22612bf74c05b51490719
Teresa Amar <zta@uw.edu>
2020-06-08 17:39:35 +1200

    UPD: update R package for Windows and Linux

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 1b221635e86a5b51995c6ecf54c70b6a3eec5b93
-#define SOURCE_CONTROL_DATE "2020-05-29"
+#define SOURCE_CONTROL_REVISION 6c47266ba724acfaf3bb48557880a3b9f380de9b
+#define SOURCE_CONTROL_DATE "2020-06-04"
 #define SOURCE_CONTROL_YEAR "2020"
-#define SOURCE_CONTROL_MONTH "May"
-#define SOURCE_CONTROL_TIME "05:01:48"
-#define SOURCE_CONTROL_VERSION "2020-05-29 05:01:48 UTC (rev. 1b22163)"
+#define SOURCE_CONTROL_MONTH "June"
+#define SOURCE_CONTROL_TIME "23:01:23"
+#define SOURCE_CONTROL_VERSION "2020-06-04 23:01:23 UTC (rev. 6c47266)"
 #endif
