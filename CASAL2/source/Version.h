a284a50af136ece26c7c3928971fc07d71bf10ba
Teresa Amar <zta@uw.edu>
2020-06-25 13:05:51 +1200

    UPD: update Test Case BCO CASAL and Casal2 files
    Former-commit-id: e1ac4a74826bc1a2a5c37fdd5d4dd7b291a20ebd

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 96f43e2949fa59bcbb9aa96edb5f5d0e406fead7
+#define SOURCE_CONTROL_REVISION 42ea2d0ab61a220e1cb3608b865bc53dadeaab33
 #define SOURCE_CONTROL_DATE "2020-06-24"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "June"
-#define SOURCE_CONTROL_TIME "02:43:34"
-#define SOURCE_CONTROL_VERSION "2020-06-24 02:43:34 UTC (rev. 96f43e2)"
+#define SOURCE_CONTROL_TIME "20:45:14"
+#define SOURCE_CONTROL_VERSION "2020-06-24 20:45:14 UTC (rev. 42ea2d0)"
 #endif
