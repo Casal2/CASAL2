5f81759d25cf01b063231490578cce9efd6c752b
Teresa Amar <zta@uw.edu>
2020-07-08 10:28:19 +1200

    UPD: update Test Case BCO report with total and discarded catch figures
    Former-commit-id: 8efb444ada59ebe45d4d8e6e1cb9c36d0b763258

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 37de0ad34bfc88cc6ebc65ed35130e3b5bda1d1c
-#define SOURCE_CONTROL_DATE "2020-07-03"
+#define SOURCE_CONTROL_REVISION 1ad359eed40a66337cacd30fefb2da17c109125a
+#define SOURCE_CONTROL_DATE "2020-07-07"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "05:22:50"
-#define SOURCE_CONTROL_VERSION "2020-07-03 05:22:50 UTC (rev. 37de0ad)"
+#define SOURCE_CONTROL_TIME "06:52:43"
+#define SOURCE_CONTROL_VERSION "2020-07-07 06:52:43 UTC (rev. 1ad359e)"
 #endif
