dfe923ff2d022d4eaa37d12677ec82c0dc448171
Teresa Amar <zta@uw.edu>
2020-07-10 13:38:39 +1200

    UPD: Update CASAL output for Test Case BCO, HOK, LIN5/6, ORH, and SBW
    Former-commit-id: dd7e099f8aa520ef7670d16da52f4e0a81de2b43

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION a336152806528feb5ec40f452f8fdf9888ddd4bf
+#define SOURCE_CONTROL_REVISION 58d29e93aab078611fbbaea7522f0d0f8b3930f4
 #define SOURCE_CONTROL_DATE "2020-07-09"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "04:46:25"
-#define SOURCE_CONTROL_VERSION "2020-07-09 04:46:25 UTC (rev. a3361528)"
+#define SOURCE_CONTROL_TIME "23:45:02"
+#define SOURCE_CONTROL_VERSION "2020-07-09 23:45:02 UTC (rev. 58d29e93)"
 #endif
