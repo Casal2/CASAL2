0b1831647cc93c5cf1ffa8186d5f52d6299e7139
Teresa Amar <zta@uw.edu>
2020-07-10 14:02:37 +1200

    UPD: update Test Case SBW report and Casal2 output
    Former-commit-id: 87198bd415e1747460e425d6522746fd849624d3

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 58d29e93aab078611fbbaea7522f0d0f8b3930f4
-#define SOURCE_CONTROL_DATE "2020-07-09"
+#define SOURCE_CONTROL_REVISION dd7e099f8aa520ef7670d16da52f4e0a81de2b43
+#define SOURCE_CONTROL_DATE "2020-07-10"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "23:45:02"
-#define SOURCE_CONTROL_VERSION "2020-07-09 23:45:02 UTC (rev. 58d29e93)"
+#define SOURCE_CONTROL_TIME "01:38:39"
+#define SOURCE_CONTROL_VERSION "2020-07-10 01:38:39 UTC (rev. dd7e099f)"
 #endif
