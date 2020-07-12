bd548a5e6421397fb7a5c734b50232f6bd8fde9d
Teresa Amar <zta@uw.edu>
2020-07-13 11:16:21 +1200

    UPD: update Test Case HAK, HOK, LIN5/6, ORH, and SBW with jenkins Windows run
    Former-commit-id: 8c3719ab22b2c754b79c2d6c886f18c0c7a74336

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 411727d6a6c84bf0c6af1976b8f04c89a940913c
+#define SOURCE_CONTROL_REVISION 465de213ddcb571297b547ad2dbe915e7f8d1242
 #define SOURCE_CONTROL_DATE "2020-07-10"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "06:37:30"
-#define SOURCE_CONTROL_VERSION "2020-07-10 06:37:30 UTC (rev. 411727d6)"
+#define SOURCE_CONTROL_TIME "06:58:08"
+#define SOURCE_CONTROL_VERSION "2020-07-10 06:58:08 UTC (rev. 465de213)"
 #endif
