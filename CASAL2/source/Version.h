56ae28d962e644fdb409af551ebba4848389bc9c
Teresa Amar <zta@uw.edu>
2020-07-13 15:32:08 +1200

    UPD: update Test Case BCO Casal2 ADOL-C model output
    Former-commit-id: 143ce32572cc5474772749794f9051f120213f0f

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 465de213ddcb571297b547ad2dbe915e7f8d1242
-#define SOURCE_CONTROL_DATE "2020-07-10"
+#define SOURCE_CONTROL_REVISION 8c3719ab22b2c754b79c2d6c886f18c0c7a74336
+#define SOURCE_CONTROL_DATE "2020-07-12"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "06:58:08"
-#define SOURCE_CONTROL_VERSION "2020-07-10 06:58:08 UTC (rev. 465de213)"
+#define SOURCE_CONTROL_TIME "23:16:21"
+#define SOURCE_CONTROL_VERSION "2020-07-12 23:16:21 UTC (rev. 8c3719ab)"
 #endif
