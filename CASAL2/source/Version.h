bb83d529c919b211b02c4d35d83a9de5f7b53040
Teresa Amar <zta@uw.edu>
2020-07-14 16:58:47 +1200

    UPD: first pass at changes for rotating abundance and biomass observations
    Former-commit-id: 90c9ecd28a6352de9bf09e10002d15da42204f9e

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 8c3719ab22b2c754b79c2d6c886f18c0c7a74336
-#define SOURCE_CONTROL_DATE "2020-07-12"
+#define SOURCE_CONTROL_REVISION 87aeb31dfee5c11efe7c4eabe8e6248e05a7c08c
+#define SOURCE_CONTROL_DATE "2020-07-13"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "23:16:21"
-#define SOURCE_CONTROL_VERSION "2020-07-12 23:16:21 UTC (rev. 8c3719ab)"
+#define SOURCE_CONTROL_TIME "03:44:42"
+#define SOURCE_CONTROL_VERSION "2020-07-13 03:44:42 UTC (rev. 87aeb31d)"
 #endif
