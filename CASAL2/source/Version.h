0b2b53eb2c7d94d39835f73932776ba576f77271
Teresa Amar <zta@uw.edu>
2020-07-20 15:52:07 +1200

    UPD: update CppAD option names to match BetaDiff option names
    Former-commit-id: bec7ae874fbbd0a242fd36d7e15c7b23e3b9918e

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION acf30f677611f51839cb801de2b499e2cdc4188c
+#define SOURCE_CONTROL_REVISION 728d7b9e3b8efd5472479cee91821daf618f959c
 #define SOURCE_CONTROL_DATE "2020-07-20"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "01:41:21"
-#define SOURCE_CONTROL_VERSION "2020-07-20 01:41:21 UTC (rev. acf30f6)"
+#define SOURCE_CONTROL_TIME "03:30:58"
+#define SOURCE_CONTROL_VERSION "2020-07-20 03:30:58 UTC (rev. 728d7b9)"
 #endif
