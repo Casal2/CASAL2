56e4748d89c69b0e88194ab684dac047bf3d640d
Teresa Amar <zta@uw.edu>
2020-07-27 11:45:23 +1200

    UPD: update User Manual Population section to build
    Former-commit-id: 19b14d0e4703a3587c71bdc7c27273f1769210ec

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 1587b8a0fdeda82b4ddaefc72bd5bf81600f5a0b
-#define SOURCE_CONTROL_DATE "2020-07-23"
+#define SOURCE_CONTROL_REVISION ce5492ed79830e67006eba7cd2144d66bff4aa43
+#define SOURCE_CONTROL_DATE "2020-07-26"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "03:41:38"
-#define SOURCE_CONTROL_VERSION "2020-07-23 03:41:38 UTC (rev. 1587b8a0)"
+#define SOURCE_CONTROL_TIME "21:57:49"
+#define SOURCE_CONTROL_VERSION "2020-07-26 21:57:49 UTC (rev. ce5492e)"
 #endif
