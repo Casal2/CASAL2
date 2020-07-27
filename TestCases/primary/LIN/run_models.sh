4795c56142d947032f367970c1fe26368f88e414
Teresa Amar <zta@uw.edu>
2020-07-27 13:38:17 +1200

    UPD: update Test Case Casal2 MPD output from Windows jenkins job
    Former-commit-id: cd2b637b592e92e3c37b85e563742917ea05319e

---
+++ TestCases/primary/LIN/run_models.sh
@@ -0,0 +1,46 @@
+# this assumes that 'release adolc', 'release betadiff', and 'release cppad' have already been built successfully
+# start in directory TestCases/primary/LIN56
+
+# cd CASAL
+# rm -f params_est.out
+# ../../../casal -e -o params_est.out > run_estimation.txt 2>&1
+
+# cd ../CASAL_sens1
+# rm -f params_est.out
+# ../../../casal -e -o params_est.out > run_estimation.txt 2>&1
+
+
+cd Casal2/betadiff_casal_flags_off
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../betadiff_casal_flags_on
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../betadiff_casal_flags_on_low_tol
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+
+cd ../cppad_casal_flags_off
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../cppad_casal_flags_on
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+
+cd ../adolc_casal_flags_off
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../adolc_casal_flags_on
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
+cd ../adolc_casal_flags_on_low_tol
+rm -f params_est.out
+../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+
