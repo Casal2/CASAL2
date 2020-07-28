214fe96b108a4fe4f31cafec3e01774cf666cbfd
Teresa Amar <zta@uw.edu>
2020-07-29 11:46:20 +1200

    UPD: update MCMC max_corr for Test Case HOK and LIN; issue #104
    Former-commit-id: cc5870fd52d95b00e8159331734e9b192ee5c7bd

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 22291c21876e10cad0e1de0ff95ecead0f8f3ba1
+#define SOURCE_CONTROL_REVISION 258dfb65d2921d37dc1360f8cbc27cbea6f5fe5d
 #define SOURCE_CONTROL_DATE "2020-07-28"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "06:26:41"
-#define SOURCE_CONTROL_VERSION "2020-07-28 06:26:41 UTC (rev. 22291c21)"
+#define SOURCE_CONTROL_TIME "08:18:28"
+#define SOURCE_CONTROL_VERSION "2020-07-28 08:18:28 UTC (rev. 258dfb65)"
 #endif
