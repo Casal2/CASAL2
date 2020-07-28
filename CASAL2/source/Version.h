afcf288fee2419b6f233fc5741faeedaf6580815
Teresa Amar <zta@uw.edu>
2020-07-28 19:42:15 +1200

    UPD: update unit test for issue #84; still need to edit Test Case observations
    Former-commit-id: 23a8bf564e6e531ad5f090014138521f29d931c9

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION b758f9d1ca409bdedbb293c39237782b3664a6d3
+#define SOURCE_CONTROL_REVISION 22291c21876e10cad0e1de0ff95ecead0f8f3ba1
 #define SOURCE_CONTROL_DATE "2020-07-28"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "06:16:47"
-#define SOURCE_CONTROL_VERSION "2020-07-28 06:16:47 UTC (rev. b758f9d1)"
+#define SOURCE_CONTROL_TIME "06:26:41"
+#define SOURCE_CONTROL_VERSION "2020-07-28 06:26:41 UTC (rev. 22291c21)"
 #endif
