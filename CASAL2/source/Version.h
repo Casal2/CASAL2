8327642ff4a7db1a40a3ad934d00980277f0c49e
Teresa Amar <zta@uw.edu>
2020-07-31 14:32:27 +1200

    UPD: minor edits to the User Manual

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 258dfb65d2921d37dc1360f8cbc27cbea6f5fe5d
-#define SOURCE_CONTROL_DATE "2020-07-28"
+#define SOURCE_CONTROL_REVISION 25cde7809390437019c58687cdc84e0533b29ceb
+#define SOURCE_CONTROL_DATE "2020-07-31"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "08:18:28"
-#define SOURCE_CONTROL_VERSION "2020-07-28 08:18:28 UTC (rev. 258dfb65)"
+#define SOURCE_CONTROL_TIME "00:38:07"
+#define SOURCE_CONTROL_VERSION "2020-07-31 00:38:07 UTC (rev. 25cde78)"
 #endif
