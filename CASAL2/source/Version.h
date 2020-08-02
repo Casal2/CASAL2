b9e16d1b002d3afd8f674ae86b8cddfc5ac5c534
Teresa Amar <zta@uw.edu>
2020-08-03 11:55:13 +1200

    UPD: update User Manual Observations with abundance/biomass table obs

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 25cde7809390437019c58687cdc84e0533b29ceb
+#define SOURCE_CONTROL_REVISION 8327642ff4a7db1a40a3ad934d00980277f0c49e
 #define SOURCE_CONTROL_DATE "2020-07-31"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "00:38:07"
-#define SOURCE_CONTROL_VERSION "2020-07-31 00:38:07 UTC (rev. 25cde78)"
+#define SOURCE_CONTROL_TIME "02:32:27"
+#define SOURCE_CONTROL_VERSION "2020-07-31 02:32:27 UTC (rev. 8327642)"
 #endif
