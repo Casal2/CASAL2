c992f5dfdad9871a7c681e35830671a6afcfb3bc
Teresa Amar <zta@uw.edu>
2020-08-11 12:48:16 +1200

    UPD: update SAMCP input file templates

--- TestCases/secondary/SAMCP.r
+++ TestCases/secondary/SAMCP.r
@@ -8,10 +8,10 @@ current_dir <- here()
 
 
 # use BetaDiff version as the default; matches Estimation.csl2
-casal2_path <- '../../BuildSystem/bin/linux/release_betadiff/casal2'
+casal2_path <- normalizePath('../../BuildSystem/bin/linux/release_betadiff/casal2')
 
 
-samcp_dir <- '../../../../noaa/NOAA-SAMCP/OM/'
+samcp_dir <- normalizePath('../../../dev/NOAA-SAMCP/OM/')
 
 load_samcp_data <- function(data_set_num = -1) {
     if (data_set_num < 1) {
@@ -47,19 +47,20 @@ load_samcp_data <- function(data_set_num = -1) {
 # dat.input are the observed values with noise
 
 # > names(sim1)
-# [1] "yr"         "SSB"        "abundance"  "biomass.mt" "N.age"      "L.age"
-# [7] "L.knum"     "L.mt"       "msy"        "F"          "FAA"
+#  [1] "yr"         "SSB"        "abundance"  "biomass.mt" "N.age"      "L.age"
+#  [7] "L.knum"     "L.mt"       "msy"        "F"          "FAA"
 # > names(dat.sim1)
 # [1] "L.obs"          "survey.obs"     "L.age.obs"      "survey.age.obs" "q"
 # > names(par.sim1)
-# [1] "nyr"               "ages"              "cv.L"              "cv.survey"
-# [5] "n.L"               "n.survey"          "logR.sd"           "R0"
-# [9] "h"                 "M"                 "Linf"              "K"
+#  [1] "nyr"               "ages"              "cv.L"              "cv.survey"
+#  [5] "n.L"               "n.survey"          "logR.sd"           "R0"
+#  [9] "h"                 "M"                 "Linf"              "K"
 # [13] "a0"                "a.lw"              "b.lw"              "A50.mat"
 # [17] "slope.mat"         "slope.sel"         "A50.sel"           "slope.sel.survey"
 # [21] "A50.sel.survey"    "len"               "W.kg"              "W.mt"
 # [25] "M.age"             "mat.age"           "proportion.female" "selex"
 # [29] "selex.survey"      "N.pr0"             "Phi.0"             "logR.resid"
+# [33] "yr"
 
 # > names(dat.input)
 # [1] "L.obs"          "survey.obs"     "L.age.obs"      "survey.age.obs" "q"
@@ -76,29 +77,33 @@ load_samcp_data <- function(data_set_num = -1) {
 # > dim(survey.sim1.age)
 # [1] 30 12
 
+
 # c(bottom, left, top, right)
 par(mar=c(4,4,2,1) + 0.1)
 
 # > plot(dat.sim1$L.obs, type='l', col='black', lwd=2, xlab='Year', ylab='Landings',
 #        ylim=c(0, max(dat.sim1$L.obs, sim1$L.mt, dat.input$L.obs)))
-# > lines(sim1$L.mt, col='blue', lwd=2)
+# > lines(sim1$L.mt, col='cyan2', lwd=2)
 # > lines(dat.input$L.obs, col='green', lwd=1)
 
 # > plot(log(dat.sim1$L.obs), type='l', col='black', lwd=2, xlab='Year', ylab='Landings (log)',
 #        ylim=log(range(c(dat.sim1$L.obs, sim1$L.mt, dat.input$L.obs))))
-# > lines(log(sim1$L.mt), col='blue', lwd=2)
+# > lines(log(sim1$L.mt), col='cyan2', lwd=2)
 # > lines(log(dat.input$L.obs), col='green', lwd=1)
 
 
 # > plot(dat.sim1$survey.obs, type='l', col='black', lwd=2, xlab='Year', ylab='Survey index',
 #        ylim=range(c(dat.sim1$survey.obs, survey.sim1, dat.input$survey.obs)))
-# > lines(survey.sim1, col='blue', lwd=2)
+# > lines(survey.sim1, col='cyan2', lwd=2)
 # > lines(dat.input$survey.obs, col='green', lwd=1)
 
 
 
 # run_dir could be run-specific
-run_dir <- current_dir
+run_dir <- file.path(current_dir, 'runs')
+if (!dir.exists(run_dir)) {
+    dir.create(run_dir, recursive=TRUE)
+}
 
 
 # load Casal2 input file templates
@@ -124,8 +129,8 @@ rep_csl2_df <- rep_csl2_template_df
 # write age range and year range to Population.csl2
 par.sim1$yr <- 2000 + sim1$yr
 
-min_year  <-  min(par.sim1$yr)
-max_year  <-  max(par.sim1$yr)
+min_year  <- min(par.sim1$yr)
+max_year  <- max(par.sim1$yr)
 num_years <- length(par.sim1$yr)
 year_vec  <- seq(min_year, max_year, 1)
 
@@ -154,15 +159,18 @@ pop_csl2_df$`catchability[survey_q]`$q$value <- as.character(dat.input$q)
 
 # write anything to Estimation.csl2?
 # est_csl2_df
+# par.sim1$logR.sdR.sd
 
 # where to use catch uncertainty?
 # dat.input$cv.L
 
 
 # write dat.input to Observation.csl2
-obs_csl2_df$`observation[survey_abundance]`$years$value       <- as.character(year_vec)
-obs_csl2_df$`observation[survey_abundance]`$obs$value         <- as.character(dat.input$survey.obs)
-obs_csl2_df$`observation[survey_abundance]`$error_value$value <- as.character(dat.input$cv.survey)
+obs_csl2_df$`observation[survey_abundance]`$years$value        <- as.character(year_vec)
+for (y in 1:num_years) {
+    obs_csl2_df$`observation[survey_abundance]`$Table$obs[[y]] <- c(as.character(dat.input$survey.obs[y]), as.character(dat.input$cv.survey))
+}
+names(obs_csl2_df$`observation[survey_abundance]`$Table$obs)   <- as.character(year_vec)
 
 obs_csl2_df$`observation[survey_prop_at_age]`$min_age$value      <- as.character(min_age)
 obs_csl2_df$`observation[survey_prop_at_age]`$max_age$value      <- as.character(max_age)
@@ -171,7 +179,7 @@ obs_csl2_df$`observation[survey_prop_at_age]`$Table$obs          <- list()
 for (y in 1:num_years) {
     obs_csl2_df$`observation[survey_prop_at_age]`$Table$obs[[y]] <- as.character(dat.input$survey.age.obs[y,])
 }
-names(obs_csl2_df$`observation[survey_prop_at_age]`$Table$obs) <- as.character(year_vec)
+names(obs_csl2_df$`observation[survey_prop_at_age]`$Table$obs)   <- as.character(year_vec)
 obs_csl2_df$`observation[survey_prop_at_age]`$Table$error_values <- as.character(rep(200, num_years))
 names(obs_csl2_df$`observation[survey_prop_at_age]`$Table$error_values) <- year_vec
 
@@ -191,6 +199,9 @@ names(obs_csl2_df$`observation[fishery_prop_at_age]`$Table$error_values) <- year
 # rep_csl2_df
 
 
+config_file <- 'config.csl2'
+file.copy(config_file, file.path(run_dir, config_file))
+
 write.csl2.file(pop_csl2_df, file.path(run_dir, 'Population.csl2'))
 write.csl2.file(est_csl2_df, file.path(run_dir, 'Estimation.csl2'))
 write.csl2.file(obs_csl2_df, file.path(run_dir, 'Observation.csl2'))
@@ -198,50 +209,60 @@ write.csl2.file(rep_csl2_df, file.path(run_dir, 'Reports.csl2'))
 
 
 
-cmd_output <- system2(file.path(casal2_path), paste('-e', '-o est_params.out', sep=' '), stdout=TRUE, stderr=TRUE, stdin='', input=NULL, wait=TRUE)
+setwd(run_dir)
+
+cmd_output <- system2(casal2_path, args=c('-e', '-o params_est.out'), stdout=TRUE, stderr=TRUE, stdin='', input=NULL, wait=TRUE)
 
 tail(cmd_output)
 
 output_filename <- file.path(run_dir, paste('est_output.', ds_idx, '.txt', sep=''))
-writeLines(cmd_output,  output_filename)
+writeLines(cmd_output, output_filename)
 
 ds_mpd <- extract.mpd(output_filename)
 
 # > names(ds_mpd)
-#  [1] "Init"                "state1"              "obj_fun"             "DQs"
-#  [5] "estimated_values"    "SSB"                 "Mortality"           "Rec"
-#  [9] "maturity_ogive"      "survey_selectivity"  "fishery_selectivity" "survey_abundance"
-# [13] "Hess"                "Covar"               "Corr"                "estimated_summary"
-# [17] "warnings_encounted"
+#  [1] "Init"                 "state1"               "obj_fun"
+#  [4] "Mortality"            "Recruitment"          "DerivedQuantities"
+#  [7] "estimated_values"     "estimated_summary"    "Qs"
+# [10] "maturity_ogive"       "growth_length_at_age" "growth_weight_at_age"
+# [13] "survey_selectivity"   "fishery_selectivity"  "survey_abundance"
+# [16] "survey_paa"           "fishery_paa"          "Hess"
+# [19] "Covar"                "Corr"                 "warnings_encounted"
+
+
+
+setwd(current_dir)
+
 
 
 ds_mpd$warnings_encounted
 
 
 
-plot.derived_quantities(ds_mpd, 'SSB')
-lines(year_vec, sim1$SSB, type='b', col='blue', pch=16)
+plot.derived_quantities(ds_mpd, 'DerivedQuantities')
+lines(year_vec, sim1$SSB, type='b', col='cyan2', pch=16)
 
 
 plot.fits(ds_mpd, 'survey_abundance')
+lines(year_vec, survey.sim1, type='b', col='cyan2', pch=16)
 
 
 plot.pressure(ds_mpd, 'Mortality', col='black', ylim=c(0, max(ds_mpd$Mortality$`fishing_pressure[Fishery]`, sim1$F)))
-lines(year_vec, sim1$F, type='b', col='blue', pch=16)
+lines(year_vec, sim1$F, type='b', col='cyan2', pch=16)
 
 
 # plots S-R, not recruitment time series
 plot.recruitment(ds_mpd, 'Recruitment')
 true_rec_vec <- sim1$N.age[,1]
-points(sim1$SSB, true_rec_vec / 1000.0, col='blue')
+points(sim1$SSB, true_rec_vec / 1000.0, col='cyan2', pch=16)
 
 
 # recruitment time series
 plot(full_year_vec, ds_mpd$Recruitment$Recruits, type='b', col='black', lwd=2, pch=20, xlab='Year', ylab='Recruits',
      ylim=range(c(ds_mpd$Recruitment$Recruits, true_rec_vec)))
 abline(h=ds_mpd$Recruitment$r0, col='black')
-lines(year_vec, true_rec_vec, type='b', lwd=2, col='blue')
-abline(h=par.sim1$R0, col='blue')
+lines(year_vec, true_rec_vec, type='b', lwd=2, col='cyan2')
+abline(h=par.sim1$R0, col='cyan2')
 
 
 plot.selectivities(ds_mpd, c('fishery_selectivity', 'survey_selectivity', 'maturity_ogive'), col=c('black', 'green', 'red'))
@@ -252,23 +273,30 @@ lines(par.sim1$mat.age / 2, col='red', lwd=2, lty=2)
 
 # plots exp(rec_dev)
 plot.ycs(ds_mpd, 'Recruitment')
-lines(year_vec - 1, exp(par.sim1$logR.resid), col='blue')
+lines(year_vec - 1, exp(par.sim1$logR.resid), col='cyan2')
 
 
 # plot growth comparisons, age-length, age-weight, and length-weight
-plot(age_vec, ds_mpd$growth_length_at_age$stock$mean_lengths$values, type='b', col='black', xlab='Age', ylab='Length (cm)',
-     ylim=c(0, max(ds_mpd$growth_length_at_age$stock$mean_lengths$values, ds_mpd$estimated_values$values$`age_length[age_len_label].Linf`,
+# omit the first column which is 'year'
+age_at_length_vec <- unlist(ds_mpd$growth_length_at_age$stock$mean_lengths[-1])
+plot(age_vec, age_at_length_vec, type='b', col='black', xlab='Age', ylab='Length (cm)',
+     ylim=c(0, max(age_at_length_vec, ds_mpd$estimated_values$values$`age_length[age_len_label].Linf`,
                    par.sim1$len / 10.0, par.sim1$Linf / 10.0)))
-lines(age_vec, par.sim1$len / 10.0, type='b', col='blue')
+lines(age_vec, par.sim1$len / 10.0, type='b', col='cyan2')
 abline(h=ds_mpd$estimated_values$values$`age_length[age_len_label].Linf`, col='black')
-abline(h=par.sim1$Linf / 10.0, col='blue')
+abline(h=par.sim1$Linf / 10.0, col='cyan2')
 
 
-plot(age_vec, ds_mpd$growth_weight_at_age$stock$mean_weights$values, type='b', col='black',
-     xlab='Age', ylab='Weight (mt)', ylim=c(0, max(ds_mpd$growth_weight_at_age$stock$mean_weights$values, par.sim1$W.mt)))
-lines(age_vec, par.sim1$W.mt, type='b', col='blue')
+# omit the first column which is 'year'
+weight_at_length_vec <- unlist(ds_mpd$growth_weight_at_age$stock$mean_weights[-1])
+plot(age_vec, weight_at_length_vec, type='b', col='black',
+     xlab='Age', ylab='Weight (mt)', ylim=c(0, max(weight_at_length_vec, par.sim1$W.mt)))
+lines(age_vec, par.sim1$W.mt, type='b', col='cyan2')
 
 
 
 ds_mpd$estimated_values
+
+
+
 
