efa0425413c617d30dc2ddc81163ebfad6c98ea4
Teresa Amar <zta@uw.edu>
2020-11-23 15:05:55 +1300

    UPD: update Examples/Simple model output

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION d7dbf7c54dac99fccb14834e14ad9df365d0c863
-#define SOURCE_CONTROL_DATE "2020-09-07"
+#define SOURCE_CONTROL_REVISION 9ea82f43413c05df68f23dacc799a84ae6182a2b
+#define SOURCE_CONTROL_DATE "2020-11-23"
 #define SOURCE_CONTROL_YEAR "2020"
-#define SOURCE_CONTROL_MONTH "September"
-#define SOURCE_CONTROL_TIME "05:41:22"
-#define SOURCE_CONTROL_VERSION "2020-09-07 05:41:22 UTC (rev. d7dbf7c)"
+#define SOURCE_CONTROL_MONTH "November"
+#define SOURCE_CONTROL_TIME "01:49:06"
+#define SOURCE_CONTROL_VERSION "2020-11-23 01:49:06 UTC (rev. 9ea82f4)"
 #endif
