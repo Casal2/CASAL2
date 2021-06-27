047c0c3230bc56aceae13d885c4e428b921979f6
Alistair Dunn <alistairdunn1@gmail.com>
2021-06-27 23:34:34 +1200

    CHG: Modifications to the Documentation builder

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION c058686700b851bf2399862ee86aa5a1c9223140
+#define SOURCE_CONTROL_REVISION e91a47a269487b99b88e912bdb4ce1b0fe993bd3
 #define SOURCE_CONTROL_DATE "2021-06-27"
 #define SOURCE_CONTROL_YEAR "2021"
 #define SOURCE_CONTROL_MONTH "June"
-#define SOURCE_CONTROL_TIME "05:19:55"
-#define SOURCE_CONTROL_VERSION "2021-06-27 05:19:55 UTC (rev. c0586867)"
+#define SOURCE_CONTROL_TIME "10:22:17"
+#define SOURCE_CONTROL_VERSION "2021-06-27 10:22:17 UTC (rev. e91a47a2)"
 #endif
