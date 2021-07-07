5f6ce847046fbf9e274051d0f8d4f6587d1d0930
Teresa Amar <zta@uw.edu>
2021-07-07 14:09:30 +1200

    UPD: more work on ProcessRemovalsByWeight

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION da66e54318f42490ad9b5cb17be04a008f4e8566
+#define SOURCE_CONTROL_REVISION 3579a4883e0fb4962e9bbac0ba43e5d49d5416e3
 #define SOURCE_CONTROL_DATE "2021-07-07"
 #define SOURCE_CONTROL_YEAR "2021"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "01:19:07"
-#define SOURCE_CONTROL_VERSION "2021-07-07 01:19:07 UTC (rev. da66e54)"
+#define SOURCE_CONTROL_TIME "02:04:12"
+#define SOURCE_CONTROL_VERSION "2021-07-07 02:04:12 UTC (rev. 3579a48)"
 #endif
