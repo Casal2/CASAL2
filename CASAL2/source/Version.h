13ea12b80a79010cfdd622b249317729f7dd8fcb
Teresa Amar <zta@uw.edu>
2021-07-26 15:58:28 +1200

    ADD: add technical specification for ProcessRemovalsByWeight

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 99d6b3ede6319f633628f786e970e575723342cf
-#define SOURCE_CONTROL_DATE "2021-07-22"
+#define SOURCE_CONTROL_REVISION b2f75cec710cb41fe4fa2d01d4d6cd87582be2d7
+#define SOURCE_CONTROL_DATE "2021-07-23"
 #define SOURCE_CONTROL_YEAR "2021"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "02:57:46"
-#define SOURCE_CONTROL_VERSION "2021-07-22 02:57:46 UTC (rev. 99d6b3ed)"
+#define SOURCE_CONTROL_TIME "04:42:35"
+#define SOURCE_CONTROL_VERSION "2021-07-23 04:42:35 UTC (rev. b2f75cec)"
 #endif
