d645c00e6ba4908076d20f39344268ef71da20ec
Teresa Amar <zta@uw.edu>
2021-08-18 14:14:10 +1200

    UPD: update Test Case run scripts to omit CppAD runs

--- TestCases/primary/BCO/run_models.sh
+++ TestCases/primary/BCO/run_models.sh
@@ -23,13 +23,13 @@ rm -f params_est.out
 ../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 
-cd ../cppad_casal_flags_off
-rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+# cd ../cppad_casal_flags_off
+# rm -f params_est.out
+# ../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
-cd ../cppad_casal_flags_on
-rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+# cd ../cppad_casal_flags_on
+# rm -f params_est.out
+# ../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 
 cd ../adolc_casal_flags_off
