0a8c843d563330e405d453cbc452ae63a6bf699e
Scott Rasmussen <scott@zaita.com>
2021-08-24 20:09:41 +1200

    DEL: Removing a bunch of temporary files

--- FrontEnd/build/CMakeFiles/casal2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for casal2.
-# This may be replaced when dependencies are built.
--- FrontEnd/build/CMakeFiles/icon.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/icon"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/icon.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
