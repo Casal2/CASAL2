1510714fa93c74bf84f160736a2cc54029103653
Teresa Amar <zta@uw.edu>
2021-08-30 12:24:58 +1200

    UPD: update TestCase primary Linux MPD and MCMC bash scripts

--- TestCases/primary/HOK/run_models.sh
+++ TestCases/primary/HOK/run_models.sh
@@ -12,35 +12,35 @@
 
 cd Casal2/betadiff_casal_flags_off
 rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+../../../../../BuildSystem/bin/_gcc/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ../betadiff_casal_flags_on
 rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+../../../../../BuildSystem/bin/_gcc/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ../betadiff_casal_flags_on_low_tol
 rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+../../../../../BuildSystem/bin/_gcc/release_betadiff/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 
 # cd ../cppad_casal_flags_off
 # rm -f params_est.out
-# ../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+# ../../../../../BuildSystem/bin/_gcc/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 # cd ../cppad_casal_flags_on
 # rm -f params_est.out
-# ../../../../../BuildSystem/bin/linux/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+# ../../../../../BuildSystem/bin/_gcc/release_cppad/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 
 cd ../adolc_casal_flags_off
 rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+../../../../../BuildSystem/bin/_gcc/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ../adolc_casal_flags_on
 rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+../../../../../BuildSystem/bin/_gcc/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
 cd ../adolc_casal_flags_on_low_tol
 rm -f params_est.out
-../../../../../BuildSystem/bin/linux/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
+../../../../../BuildSystem/bin/_gcc/release_adolc/casal2 -e -o params_est.out > run_estimation.txt 2>&1
 
