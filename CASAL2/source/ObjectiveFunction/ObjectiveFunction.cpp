0aed506818e4c059ec0195b06c3217d32de4a242
Craig Marsh <Craig.Marsh@niwa.co.nz>
2021-11-04 13:14:27 +1300

    ADD: restore and prepare transform methods to objective function call

--- CASAL2/source/ObjectiveFunction/ObjectiveFunction.cpp
+++ CASAL2/source/ObjectiveFunction/ObjectiveFunction.cpp
@@ -112,6 +112,7 @@ void ObjectiveFunction::CalculateScore() {
   /**
    * Get the scores from each of the estimate priors
    */
+  model_->managers()->addressable_transformation()->PrepareForObjectiveFunction(); // prior applies to the transformed variable
   vector<Estimate*> estimates = model_->managers()->estimate()->objects();
   priors_                     = 0.0;
   for (Estimate* estimate : estimates) {
@@ -129,6 +130,7 @@ void ObjectiveFunction::CalculateScore() {
     score_ += new_score.score_;
     priors_ += new_score.score_;
   }
+  model_->managers()->addressable_transformation()->RestoreForObjectiveFunction(); // revert any transformations that were required to change for prior
 
   /**
    * Get the score from each additional prior
