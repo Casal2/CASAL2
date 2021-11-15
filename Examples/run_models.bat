eebbcfa2f2261ace1fabeb071e012e42f35083ef
Alistair Dunn <alistairdunn1@gmail.com>
2021-11-16 11:45:51 +1300

    UPD: Updated examples with MCMCs and tabular mode output

--- Examples/run_models.bat
+++ Examples/run_models.bat
@@ -3,13 +3,17 @@
 cd Simple
 ..\..\BuildSystem\Casal2\casal2 -E MPD.dat -o free.dat > estimate.log 2> estimate.err
 ..\..\BuildSystem\Casal2\casal2 -r -i free.dat > run.log 2> run.err
+..\..\BuildSystem\Casal2\casal2 -r -i free.dat -t > run.tab 2> run.tab.err
 ..\..\BuildSystem\Casal2\casal2 -p -i free.dat -o profile.dat > profile.log 2> profile.err
+rm *.1
 ..\..\BuildSystem\Casal2\casal2 -M MPD.dat -g 0 > MCMC.log 2> MCMC.err
 
 cd ..\2stock
 ..\..\BuildSystem\Casal2\casal2 -E MPD.dat -o free.dat > estimate.log 2> estimate.err
 ..\..\BuildSystem\Casal2\casal2 -r -i free.dat > run.log 2> run.err
-..\..\BuildSystem\Casal2\casal2 -p -i free.dat -o profile.dat > profile.log 2> profile.err
+..\..\BuildSystem\Casal2\casal2 -r -i free.dat -t > run.tab 2> run.tab.err
+rem ..\..\BuildSystem\Casal2\casal2 -p -i free.dat -o profile.dat > profile.log 2> profile.err
+rm *.1
 ..\..\BuildSystem\Casal2\casal2 -M MPD.dat -g 0 > MCMC.log 2> MCMC.err
 
 cd ..
