30f83a19580fde33717337f5fe6dc539d2ec6bbe
Teresa Amar <zta@uw.edu>
2021-11-16 13:43:43 +1300

    UPD: update HOK Test Case input files for parameter transformation

--- Examples/run_models.sh
+++ Examples/run_models.sh
@@ -1,14 +1,15 @@
+#!/bin/sh
 
 cd Simple
-..\..\BuildSystem\Casal2\casal2 -E MPD.dat -o free.dat > estimate.log >& estimate.err 
-..\..\BuildSystem\Casal2\casal2 -r -i free.dat > run.log >& run.err
-..\..\BuildSystem\Casal2\casal2 -p -i free.dat -o profile.dat > profile.log >& profile.err
-..\..\BuildSystem\Casal2\casal2 -M MPD.dat -g 0 > MCMC.log >& MCMC.err
+../../BuildSystem/Casal2/casal2 -E MPD.dat -o free.dat > estimate.log >& estimate.err
+../../BuildSystem/Casal2/casal2 -r -i free.dat > run.log >& run.err
+../../BuildSystem/Casal2/casal2 -p -i free.dat -o profile.dat > profile.log >& profile.err
+../../BuildSystem/Casal2/casal2 -M MPD.dat -g 0 > MCMC.log >& MCMC.err
 
-cd ..\2stock
-..\..\BuildSystem\Casal2\casal2 -E MPD.dat -o free.dat > estimate.log >& estimate.err
-..\..\BuildSystem\Casal2\casal2 -r -i free.dat > run.log >& run.err
-..\..\BuildSystem\Casal2\casal2 -p -i free.dat -o profile.dat > profile.log >& profile.err
-..\..\BuildSystem\Casal2\casal2 -M MPD.dat -g 0 > MCMC.log >& MCMC.err
+cd ../2stock
+../../BuildSystem/Casal2/casal2 -E MPD.dat -o free.dat > estimate.log >& estimate.err
+../../BuildSystem/Casal2/casal2 -r -i free.dat > run.log >& run.err
+../../BuildSystem/Casal2/casal2 -p -i free.dat -o profile.dat > profile.log >& profile.err
+../../BuildSystem/Casal2/casal2 -M MPD.dat -g 0 > MCMC.log >& MCMC.err
 
 cd ..
