d983c9f806bb46285fee0346573160cc3f11786d
Alistair Dunn <alistairdunn1@gmail.com>
2021-11-18 11:12:03 +1300

    UPD: Fix MH MCMC reporting of objective function values

--- CASAL2/source/MCMCs/Common/RandomWalkMetropolisHastings.cpp
+++ CASAL2/source/MCMCs/Common/RandomWalkMetropolisHastings.cpp
@@ -48,14 +48,19 @@ void RandomWalkMetropolisHastings::DoExecute(shared_ptr<ThreadPool> thread_pool)
   LOG_MEDIUM() << "Covariance matrix has rows = " << covariance_matrix_.size1() << " and cols = " << covariance_matrix_.size2();
   LOG_MEDIUM() << "Estimate Count: " << estimate_count_;
 
-  vector<double>     previous_candidates = candidates_;
-  ObjectiveFunction& obj_function        = model_->objective_function();
-  double             previous_score      = obj_function.score();
+  vector<double>     current_candidates = candidates_;
+  ObjectiveFunction& obj_function       = model_->objective_function();
+  double             current_score      = obj_function.score();
 
   mcmc::ChainLink last_link;
 
-  double score           = 0.0;
-  int    invalid_counter = 0;
+  double proposed_score            = 0.0;
+  double current_likelihoods       = 0.0;
+  double current_priors            = 0.0;
+  double current_penalties         = 0.0;
+  double current_additional_priors = 0.0;
+  double current_jacobians         = 0.0;
+  int    invalid_counter           = 0;
 
   do {
     // std::cout << ".";
@@ -65,34 +70,40 @@ void RandomWalkMetropolisHastings::DoExecute(shared_ptr<ThreadPool> thread_pool)
     UpdateStepSize();
     UpdateCovarianceMatrix();
     GenerateNewCandidates();
-    score = 0.0;
 
     if (WithinBounds()) {
       ++jumps_;
       ++jumps_since_adapt_;
 
-      for (unsigned i = 0; i < candidates_.size(); ++i) estimates_[i]->set_value(candidates_[i]);
+      for (unsigned i = 0; i < candidates_.size(); ++i)
+        estimates_[i]->set_value(candidates_[i]);
 
       model_->FullIteration();
       obj_function.CalculateScore();
-      score = obj_function.score();
+      proposed_score = obj_function.score();
 
       Double ratio = 1.0;
-      if (score >= previous_score) {
-        ratio = exp(previous_score - score);
+      if (proposed_score >= current_score) {
+        ratio = exp(current_score - proposed_score);
       }
 
       // Check if we accept this jump
       // double rng_uniform = rng.uniform();
       if (math::IsEqual(ratio, 1.0) || rng.uniform() < ratio) {
-        LOG_MEDIUM() << "Accept: Possible. Iteration = " << jumps_ << ", score = " << score << " Previous score " << previous_score;
+        LOG_MEDIUM() << "Accept: Possible. Iteration = " << jumps_ << ", score = " << proposed_score << " current score " << current_score;
         successful_jumps_++;
         successful_jumps_since_adapt_++;
-        previous_score      = score;
-        previous_candidates = candidates_;
+        current_score             = proposed_score;
+        current_candidates        = candidates_;
+        current_likelihoods       = obj_function.likelihoods();
+        current_priors            = obj_function.priors();
+        current_penalties         = obj_function.penalties();
+        current_additional_priors = obj_function.additional_priors();
+        current_jacobians         = obj_function.jacobians();
+
       } else {  // reject this jump reset
-        candidates_ = previous_candidates;
-        LOG_MEDIUM() << "Reject: Possible. Iteration = " << jumps_ << ", score = " << score << " Previous score " << previous_score;
+        candidates_ = current_candidates;
+        LOG_MEDIUM() << "Reject: Possible. Iteration = " << jumps_ << ", score = " << proposed_score << " current score " << current_score;
       }
 
       if (jumps_ % keep_ == 0) {
@@ -102,12 +113,12 @@ void RandomWalkMetropolisHastings::DoExecute(shared_ptr<ThreadPool> thread_pool)
           mcmc_state_ = PARAM_BURN_IN;
         mcmc::ChainLink new_link{.iteration_                   = jumps_,
                                  .mcmc_state_                  = mcmc_state_,
-                                 .score_                       = obj_function.score(),
-                                 .likelihood_                  = obj_function.likelihoods(),
-                                 .prior_                       = obj_function.priors(),
-                                 .penalty_                     = obj_function.penalties(),
-                                 .additional_priors_           = obj_function.additional_priors(),
-                                 .jacobians_                   = obj_function.jacobians(),
+                                 .score_                       = current_score,
+                                 .likelihood_                  = current_likelihoods,
+                                 .prior_                       = current_priors,
+                                 .penalty_                     = current_penalties,
+                                 .additional_priors_           = current_additional_priors,
+                                 .jacobians_                   = current_jacobians,
                                  .acceptance_rate_             = double(successful_jumps_) / double(jumps_),
                                  .acceptance_rate_since_adapt_ = double(successful_jumps_since_adapt_) / double(jumps_since_adapt_),
                                  .step_size_                   = step_size_,
@@ -117,10 +128,10 @@ void RandomWalkMetropolisHastings::DoExecute(shared_ptr<ThreadPool> thread_pool)
         // LOG_MEDIUM() << "Storing: Successful Jumps " << successful_jumps_ << " Jumps : " << jumps_;
         model_->managers()->report()->Execute(model_, State::kIterationComplete);
       }
-    } else {  // Reject this attempt
+    } else {  // Attempt was out of bounds, and hence reset to last in-bounds candidate, as used by GenerateNewCandidates
       ++invalid_counter;
-      candidates_ = previous_candidates;
-      LOG_MEDIUM() << "Reject: Bounds. Iteration = " << jumps_ << ", score = " << score << " Previous score " << previous_score << ". There were " << invalid_counter
+      candidates_ = current_candidates;
+      LOG_MEDIUM() << "Reject: Bounds. Iteration = " << jumps_ << ", score = " << proposed_score << " current score " << current_score << ". There were " << invalid_counter
                    << " invalid jumps";
     }
   } while (jumps_ < length_);
