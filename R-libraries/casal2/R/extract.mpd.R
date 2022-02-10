9955b3a0a060dc9429453049ca6c19681530564f
Craig Marsh <Craig.Marsh@niwa.co.nz>
2022-02-10 17:40:25 +1300

    FIX: case mismatch between R library and reports

--- R-libraries/casal2/R/extract.mpd.R
+++ R-libraries/casal2/R/extract.mpd.R
@@ -53,7 +53,7 @@
       }
     }
 
-    multi_year_reports <- c("partition", "PartitionBiomass", "PartitionMeanWeight", "age_length")
+    multi_year_reports <- c("Partition", "PartitionBiomass", "PartitionMeanWeight", "age_length")
     result <- list()
     for (i in 1:counter) {
       header <- split.header(temp[i])
