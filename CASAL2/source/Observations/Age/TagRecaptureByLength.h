e169fdf3c328a2f08ab92c8c39f2775f4e8ec7b8
Craig Marsh <Craig.Marsh@niwa.co.nz>
2022-02-15 20:13:01 +1300

    FIX: length bin input for obs tag-by-length

--- CASAL2/source/Observations/Age/TagRecaptureByLength.h
+++ CASAL2/source/Observations/Age/TagRecaptureByLength.h
@@ -53,7 +53,6 @@ protected:
   Double                tolerance_  = 0.0;
   vector<Double>        process_error_values_;
   map<unsigned, Double> process_errors_by_year_;
-  vector<double>        length_bins_input_;
   vector<double>        length_bins_;
   bool                  length_plus_ = false;
   string                ageing_error_label_;
