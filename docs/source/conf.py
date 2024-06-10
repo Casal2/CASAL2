e240df5641dc4b9b8a6612b39fd3dafe57384f9f
Scott Rasmussen (Zaita) <scott@zaita.com>
2024-06-10 20:02:53 +1200

    ADD: New files for ReadTheDocs

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Casal2'
+copyright = '2024, Scott Rasmussen'
+author = 'Scott Rasmussen'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'groundwork'
+html_static_path = ['_static']
+html_css_files = [ 'mycss.css' ]
